be44a88ae0f33c226cd9743191fa350c01937859
Andrew Davison <andrew@infradig.com>
2020-09-23 22:30:23 +1000

    Misc

--- builtins.c
+++ builtins.c
@@ -60,7 +60,7 @@ void throw_error(query *q, cell *c, const char *err_type, const char *expected)
 		err_type = "instantiation_error";
 		snprintf(dst2, len2, "error(%s,%s/%u)", err_type, GET_STR(q->st.curr_cell), q->st.curr_cell->arity);
 	} else if (!strcmp(err_type, "type_error"))
-		snprintf(dst2, len2, "error(%s(%s,%s/%u))", err_type, expected, dst, c->arity);
+		snprintf(dst2, len2, "error(%s(%s,%s))", err_type, expected, dst);
 	else
 		snprintf(dst2, len2, "error(%s(%s,%s/%u),%s/%u)", err_type, expected, dst, c->arity, GET_STR(q->st.curr_cell), q->st.curr_cell->arity);
 
