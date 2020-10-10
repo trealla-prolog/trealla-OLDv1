0e8d37e08cb634314e2acc30de5027f9b89fb852
Andrew Davison <andrew@infradig.com>
2020-10-10 22:49:29 +1000

    Fix quoted atom conatining ':' bug, issue #48

--- tests/issues/issue048.pl
+++ tests/issues/issue048.pl
@@ -2,5 +2,6 @@
 
 main :-
 	assertz('<https://josd.github.io/retina#p>'('<https://josd.github.io/retina#s>', '<https://josd.github.io/retina#o>')),
+	'<https://josd.github.io/retina#p>'(S, O),
 	writeln(ok),
 	halt.
