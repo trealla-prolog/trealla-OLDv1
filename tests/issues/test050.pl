5496e989b279762840c4599c75cbaa105dcb4fc8
Andrew Davison <andrew@infradig.com>
2020-10-11 13:58:10 +1000

    Fix for heap alloc, issue #50

---
+++ tests/issues/test050.pl
@@ -0,0 +1,8 @@
+:-initialization(main).
+
+main :-
+	(between(1, 100000, _),
+		assertz(hello(there)), false) ;
+		setof(X, hello(X), Ls),
+		writeln(Ls),
+		halt.
---
+++ tests/tests/test068.pl
@@ -0,0 +1,5 @@
+main :-
+	limit(5, offset(5, between(1,20,I))), writeln(I), fail.
+main :- halt.
+
+:- initialization(main).
