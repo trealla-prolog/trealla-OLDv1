f81519fe77ee31430c0e2e1dff1ef43b033742ce
Andrew Davison <andrew@infradig.com>
2020-10-11 10:02:48 +1000

    Fix for univ, issue #51

---
+++ tests/issues/issue051.pl
@@ -0,0 +1,13 @@
+:-initialization(main).
+
+main :-
+	a(b,c) =.. [_],
+	halt.
+main :-
+	writeln(false),
+	div([-10,0],fois([-4,0],[1,0])) =.. [F,X,Y],
+	writeln(F),
+	writeln(X),
+	writeln(Y),
+	writeln(ok),
+	halt.
