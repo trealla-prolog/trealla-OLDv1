79c557dc864ea5aa55a44fa713a1f16820f18db4
Andrew Davison <andrew@infradig.com>
2020-10-14 18:22:34 +1000

    Fix maplist, issue #61

--- library/apply.pl
+++ library/apply.pl
@@ -1,23 +1,54 @@
 maplist(_, []).
-maplist(P, [X1|X1s]) :- call(P, X1), maplist(P, X1s).
+maplist(P, [X1|X1s]) :-
+	copy_term(P, TMP_P), P=TMP_P,
+	copy_term(X1, TMP_X1), X1=TMP_X1,
+	call(TMP_P, TMP_X1),
+	maplist(P, X1s).
 
 maplist(_, [], []).
-maplist(P, [X1|X1s], [X2|X2s]) :- call(P, X1, X2), maplist(P, X1s, X2s).
+maplist(P, [X1|X1s], [X2|X2s]) :-
+	copy_term(P, TMP_P), P=TMP_P,
+	copy_term(X1, TMP_X1), X1=TMP_X1,
+	copy_term(X2, TMP_X2), X2=TMP_X2,
+	call(TMP_P, TMP_X1, TMP_X2),
+	maplist(P, X1s, X2s).
 
 maplist(_, [], [], []).
-maplist(P, [X1|X1s], [X2|X2s], [X3|X3s]) :- call(P, X1, X2, X3), maplist(P, X1s, X2s, X3s).
+maplist(P, [X1|X1s], [X2|X2s], [X3|X3s]) :-
+	copy_term(P, TMP_P), P=TMP_P,
+	copy_term(X1, TMP_X1), X1=TMP_X1,
+	copy_term(X2, TMP_X2), X2=TMP_X2,
+	copy_term(X3, TMP_X3), X3=TMP_X3,
+	call(TMP_P, TMP_X1, TMP_X2, TMP_X3),
+	maplist(P, X1s, X2s, X3s).
 
 maplist(_, [], [], [], []).
-maplist(P, [X1|X1s], [X2|X2s], [X3|X3s], [X4|X4s]) :- call(P, X1, X2, X3, X4), maplist(P, X1s, X2s, X3s, X4s).
+maplist(P, [X1|X1s], [X2|X2s], [X3|X3s], [X4|X4s]) :-
+	copy_term(P, TMP_P), P=TMP_P,
+	copy_term(X1, TMP_X1), X1=TMP_X1,
+	copy_term(X2, TMP_X2), X2=TMP_X2,
+	copy_term(X3, TMP_X3), X3=TMP_X3,
+	copy_term(X4, TMP_X4), X3=TMP_X4,
+	call(TMP_P, TMP_X1, TMP_X2, TMP_X3, TMP_X4),
+	maplist(P, X1s, X2s, X3s, X4s).
+
+% TO-DO...
 
 spawnlist(_, []) :- wait.
-spawnlist(P, [X1|X1s]) :- spawn(P, X1), spawnlist(P, X1s).
+spawnlist(P, [X1|X1s]) :-
+	spawn(P, X1),
+	spawnlist(P, X1s).
 
 spawnlist(_, [], []) :- wait.
-spawnlist(P, [X1|X1s], [X2|X2s]) :- spawn(P, X1, X2), spawnlist(P, X1s, X2s).
+spawnlist(P, [X1|X1s], [X2|X2s]) :-
+	spawn(P, X1, X2), spawnlist(P, X1s, X2s).
 
 spawnlist(_, [], [], []) :- wait.
-spawnlist(P, [X1|X1s], [X2|X2s], [X3|X3s]) :- spawn(P, X1, X2, X3), spawnlist(P, X1s, X2s, X3s).
+spawnlist(P, [X1|X1s], [X2|X2s], [X3|X3s]) :-
+	spawn(P, X1, X2, X3),
+	spawnlist(P, X1s, X2s, X3s).
 
 spawnlist(_, [], [], [], []) :- wait.
-spawnlist(P, [X1|X1s], [X2|X2s], [X3|X3s], [X4|X4s]) :- spawn(P, X1, X2, X3, X4), spawnlist(P, X1s, X2s, X3s, X4s).
+spawnlist(P, [X1|X1s], [X2|X2s], [X3|X3s], [X4|X4s]) :-
+	spawn(P, X1, X2, X3, X4),
+	spawnlist(P, X1s, X2s, X3s, X4s).
--- tests/issues/test061.pl
+++ tests/issues/test061.pl
@@ -1,8 +1,10 @@
 :- initialization(main).
 
 main :-
-    maplist(col([[1,2],[3,4]]), [1,2], X),
-    writeln(X),
+    maplist(col([[1,2],[3,4]]), [1,2], X1),
+    writeln(X1),
+    maplist(col([[A,2],[3,A]]), [1,2], X2),
+    writeln(X2),
     halt.
 
 col(Matrix, N, Column) :-
