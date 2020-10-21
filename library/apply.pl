46bc860432e1045825f7e8c52e818a8e6a9b5eb1
Andrew Davison <andrew@infradig.com>
2020-10-21 15:31:02 +1000

    Misc

--- library/apply.pl
+++ library/apply.pl
@@ -13,42 +13,42 @@ qsort([X|L], R, R0) :-
 qsort([], R, R).
 */
 
-maplist(_, []).
+maplist(_, []) :- !.
 maplist(P, [X1|X1s]) :-
 	call(P, X1),
 	maplist(P, X1s).
 
-maplist(_, [], []).
+maplist(_, [], []) :- !.
 maplist(P, [X1|X1s], [X2|X2s]) :-
 	call(P, X1, X2),
 	maplist(P, X1s, X2s).
 
-maplist(_, [], [], []).
+maplist(_, [], [], []) :- !.
 maplist(P, [X1|X1s], [X2|X2s], [X3|X3s]) :-
 	call(P, X1, X2, X3),
 	maplist(P, X1s, X2s, X3s).
 
-maplist(_, [], [], [], []).
+maplist(_, [], [], [], []) :- !.
 maplist(P, [X1|X1s], [X2|X2s], [X3|X3s], [X4|X4s]) :-
 	call(P, X1, X2, X3, X4),
 	maplist(P, X1s, X2s, X3s, X4s).
 
-spawnlist(_, []) :- wait.
+spawnlist(_, []) :- !, wait.
 spawnlist(P, [X1|X1s]) :-
 	spawn(P, X1),
 	spawnlist(P, X1s).
 
-spawnlist(_, [], []) :- wait.
+spawnlist(_, [], []) :- !, wait.
 spawnlist(P, [X1|X1s], [X2|X2s]) :-
 	spawn(P, X1, X2),
 	spawnlist(P, X1s, X2s).
 
-spawnlist(_, [], [], []) :- wait.
+spawnlist(_, [], [], []) :- !, wait.
 spawnlist(P, [X1|X1s], [X2|X2s], [X3|X3s]) :-
 	spawn(P, X1, X2, X3),
 	spawnlist(P, X1s, X2s, X3s).
 
-spawnlist(_, [], [], [], []) :- wait.
+spawnlist(_, [], [], [], []) :- !, wait.
 spawnlist(P, [X1|X1s], [X2|X2s], [X3|X3s], [X4|X4s]) :-
 	spawn(P, X1, X2, X3, X4),
 	spawnlist(P, X1s, X2s, X3s, X4s).
