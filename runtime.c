2f55ceda8816265ea38d3c1596ad3b9499e098a5
Andrew Davison <andrew@infradig.com>
2020-11-01 09:25:05 +1000

    Fix univ/2 for issue #81

--- runtime.c
+++ runtime.c
@@ -538,7 +538,7 @@ unsigned create_vars(query *q, unsigned cnt)
 	} else if ((g->overflow + (g->nbr_vars-g->nbr_slots)) == q->st.sp) {
 		q->st.sp += cnt;
 	} else {
-		assert(0);
+		//assert(0);
 		idx_t save_overflow = g->overflow;
 		g->overflow = q->st.sp;
 		idx_t cnt2 = g->nbr_vars-g->nbr_slots;
