68c8559f95d1bc2fe97a7e13204100e9ba6e28ad
Andrew Davison <andrew@infradig.com>
2020-11-14 11:12:51 +1000

    Fix OSX compilation with NOLDLIBS=1, see PMs comment issue #99

--- parse.c
+++ parse.c
@@ -1003,7 +1003,7 @@ query *create_query(module *m, int is_task)
 		for (int i = 0; i < MAX_QUEUES; i++)
 			q->q_size[i] = is_task ? INITIAL_NBR_QUEUE/10 : INITIAL_NBR_QUEUE;
 
-		if (errno) {
+		if (!q->frames || !q->slots || !q->choices || !q->trails) {
 			destroy_query (q);
 			q = NULL;
 		}
