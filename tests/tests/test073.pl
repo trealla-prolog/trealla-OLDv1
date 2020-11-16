daf1f451f54758a987efbef537e7e75e49357f3d
Andrew Davison <andrew@infradig.com>
2020-11-16 11:58:14 +1000

    Misc

---
+++ tests/tests/test073.pl
@@ -0,0 +1,16 @@
+:- initialization(main).
+
+main :-
+	X = {a:b},
+	write(X), nl,
+	Y = {}(a:b),
+	write(Y), nl,
+	Z = :(a,b),
+	write(Z), nl,
+	A = a:b,
+	write(A), nl,
+	B = (a:b),
+	write(B), nl,
+	halt.
+
+
