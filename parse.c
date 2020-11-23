37cf830adb4e4444313d539999c8332afb07a042
Andrew Davison <andrew@infradig.com>
2020-11-23 14:35:13 +1000

    Work on some memory leaks

--- parse.c
+++ parse.c
@@ -935,6 +935,7 @@ void destroy_query(query *q)
 	for (int i = 0; i < MAX_QUEUES; i++)
 		free(q->queue[i]);
 
+#if 0
 	slot *e = q->slots;
 
 	for (idx_t i = 0; i < q->st.sp; i++, e++) {
@@ -943,6 +944,7 @@ void destroy_query(query *q)
 		if (is_blob(c) && !is_const_cstring(c))
 			free(c->val_str);
 	}
+#endif
 
 	free(q->slots);
 	free(q->frames);
