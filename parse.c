48b3279a9cd5975281b27f4d5e3d3c89f978e49f
Andrew Davison <andrew@infradig.com>
2020-11-28 08:47:56 +1000

    Cleanup bagof/3 code working on bug

--- parse.c
+++ parse.c
@@ -3318,19 +3318,19 @@ module *create_module(const char *name)
 			"mmerge(R1, R2, R).");
 
 		make_rule(m, "bagof(T,G,B) :- "				\
-			"copy_term('$bagof'(T,G,_),TMP_G),"		\
-			"TMP_G,"					\
+			"copy_term('$bagof'(T,G,B),TMP_G),"		\
+			"TMP_G,"								\
 			"'$bagof'(T,G,B)=TMP_G.");
 
 		make_rule(m, "setof(T,G,B) :- "				\
-			"copy_term('$bagof'(T,G,_),TMP_G),"		\
-			"TMP_G,"					\
+			"copy_term('$bagof'(T,G,B),TMP_G),"		\
+			"TMP_G,"								\
 			"'$bagof'(T,G,TMP_B)=TMP_G,"			\
 			"sort(TMP_B,B).");
 
 		make_rule(m, "catch(G,E,C) :- "				\
 			"copy_term('$catch'(G,E,C),TMP_G),"		\
-			"'$catch'(G,E,C)=TMP_G,"			\
+			"'$catch'(G,E,C)=TMP_G,"				\
 			"TMP_G.");
 
 		make_rule(m, "call(G,P1) :- "				\
--- scratch/y.pl
+++ scratch/y.pl
@@ -1,5 +1,13 @@
-:- dynamic(p/1).
+:- initialization(main).
 
-p(a) :-
-    assertz(p(b)),
-    p(b).
+d(a(_), foo/1, 1).
+d(a(_), bar/2, 2).
+d(a(_), baz/3, 5).
+
+main :-
+	D = _,
+	bagof(N, d(D, F/A, N), Ns),
+	writeq(D-F/A-Ns), nl,
+	fail.
+main :-
+	halt.
