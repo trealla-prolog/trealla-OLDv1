3b7e1e319879b0ffc411352d54cb3b4b2a8b0cd6
Andrew Davison <andrew@infradig.com>
2020-12-06 13:02:30 +1000

    Work on exceptions

--- builtins.c
+++ builtins.c
@@ -5224,23 +5224,27 @@ prolog_state throw_error(query *q, cell *c, const char *err_type, const char *ex
 		snprintf(dst2, len2+1, "error(%s(%s,%s),%s/%u).", err_type, expected, dst, GET_STR(q->st.curr_cell), q->st.curr_cell->arity);
 	}
 
-	//printf("*** %s\n", dst2);
+	printf("*** %s\n", dst2);
 
-	parser *p = q->m->p;
+	parser *p = create_parser(q->m);
 	p->srcptr = dst2;
-	p->skip = true;
+	frame *g = GET_FRAME(q->st.curr_frame);
+	p->read_term = g->nbr_vars;
 	parser_tokenize(p, false, false);
-	cell *e = p->t->cells;
 
-	//printf("*** "); print_term(q, stdout, e, q->st.curr_frame, 1); printf("\n");
+	if (!create_vars(q, p->nbr_vars))
+		return throw_error(q, c, "resource_error", "too_many_vars");
+
+	cell *e = deep_copy_to_heap(q, p->t->cells, q->st.curr_frame, false);
+
+	printf("*** "); print_term(q, stdout, e, q->st.curr_frame, 1); printf("\n");
 
 	prolog_state ok = pl_failure;
 
 	if (find_exception_handler(q, e, q->st.curr_frame))
 		ok = fn_iso_catch_3(q);
 
-	//clear_term(p->t);
-	p->t->cidx = 0;
+	destroy_parser(p);
 	free(dst2);
 	free(dst);
 	return ok;
