f70df27b5da65508da9b02056aadac6bf904732b
Andrew Davison <andrew@infradig.com>
2020-12-10 10:16:21 +1000

    Work on open_4

--- internal.h
+++ internal.h
@@ -62,7 +62,7 @@ typedef uint32_t idx_t;
 #define MAX_ARITY UCHAR_MAX
 #define MAX_USER_OPS 200
 #define MAX_QUEUES 16
-#define MAX_STREAMS 64
+#define MAX_STREAMS 1024
 #define MAX_DEPTH 10000
 
 #define STREAM_BUFLEN 1024
