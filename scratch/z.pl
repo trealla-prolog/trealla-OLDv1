e9aae5e4c439bf95fe32c052fad35826d8d406ef
Andrew Davison <andrew@infradig.com>
2020-12-13 18:37:16 +1000

    Work in progress

--- scratch/z.pl
+++ scratch/z.pl
@@ -1,4 +1,17 @@
+elk(X) :- moose(X).
 
+:- dynamic(legs/2).
+legs(A, 4) :- animal(A).
+legs(octopus, 8).
 legs(A, 6) :- insect(A).
-legs(A, 7) :- A, call(A).
+legs(spider, 8).
+legs(B, 2) :- bird(B).
+
+:- dynamic(insect/1).
+insect(ant).
+insect(bee).
+
+:- dynamic(foo/1).
+foo(X) :- call(X) -> call(X).
+foo(X) :- call(X), call(X).
 
