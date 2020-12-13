03c4cf6d9d49e5dae5c350e75b35ceb006a6f13e
Andrew Davison <andrew@infradig.com>
2020-12-13 10:04:35 +1000

    Work on clause_2

--- scratch/z.pl
+++ scratch/z.pl
@@ -1,15 +1,1 @@
-- dynamic(cat/0).
-cat.
-
-:- dynamic(dog/0).
-dog :- true.
-
-elk(X) :- moose(X).
-
-:- dynamic(legs/2).
-legs(A, 6) :- insect(A).
-legs(A, 7) :- A, call(A), A, call(A), A.
-
-:- dynamic(insect/1).
-insect(ant).
-insect(bee).
+test(A) :- (A ; A ), A.
