d80917f6294cf769346422bacacd2b9deafb0f7c
Andrew Davison <andrew@infradig.com>
2020-12-18 20:42:57 +1000

    Work on mod_2 & now runs clean

--- builtins.c
+++ builtins.c
@@ -4966,7 +4966,10 @@ static USE_RESULT prolog_state fn_iso_mod_2(query *q)
 		if (p2.val_num == 0)
 			return throw_error(q, &p1, "evaluation_error", "zero_divisor");
 
-		q->accum.val_num = llabs((long long)(p1.val_num % p2.val_num));
+		q->accum.val_num = (long long)(p1.val_num % p2.val_num);
+
+		if ((p1.val_num < 0) || (p2.val_num <0))
+			q->accum.val_num = -q->accum.val_num;
 		q->accum.val_type = TYPE_INTEGER;
 	} else if (is_variable(&p1) || is_variable(&p2)) {
 		return throw_error(q, &p1, "instantiation_error", "not_sufficiently_instantiated");
