dbbec1a5f84a1af59f6757492638e7bdeba9a315
Andrew Davison <andrew@infradig.com>
2020-12-21 12:48:40 +1000

    Revert: Fix use_module with atom arg

--- parse.c
+++ parse.c
@@ -1268,7 +1268,7 @@ static void directives(parser *p, term *t)
 
 	if (!strcmp(dirname, "use_module") && (c->arity == 1)) {
 		cell *p1 = c + 1;
-		if (!is_atom(p1)) return;
+		if (!is_literal(p1)) return;
 		const char *name = PARSER_GET_STR(p1);
 		char dstbuf[1024*2];
 
