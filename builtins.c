754d75f8a7a63811ecfc605442ceb68a36d6ef97
Andrew Davison <andrew@infradig.com>
2021-02-13 09:30:49 +1000

    Remove redundant choice-point in call/0

--- builtins.c
+++ builtins.c
@@ -5024,6 +5024,7 @@ USE_RESULT pl_state fn_call_0(query *q, cell *p1)
 
 	idx_t nbr_cells = 0 + p1->nbr_cells;
 	make_call(q, tmp+nbr_cells);
+	//may_error(make_barrier(q));
 	q->st.curr_cell = tmp;
 	return pl_success;
 }
@@ -5145,7 +5146,7 @@ static USE_RESULT pl_state fn_iso_ifthen_2(query *q)
 	make_structure(tmp+nbr_cells++, g_cut_s, fn_local_cut_0, 0, 0);
 	nbr_cells += copy_cells(tmp+nbr_cells, p2, p2->nbr_cells);
 	make_call(q, tmp+nbr_cells);
-	may_error(make_barrier(q));			// Why needed?
+	may_error(make_barrier(q));
 	q->st.curr_cell = tmp;
 	return pl_success;
 }
@@ -5172,6 +5173,9 @@ static USE_RESULT pl_state do_ifthenelse(query *q, cell *p1, cell *p2, cell *p3)
 
 static USE_RESULT pl_state fn_if_2(query *q)
 {
+	if (q->retry)
+		return pl_failure;
+
 	GET_FIRST_ARG(p1,callable);
 	GET_NEXT_ARG(p2,callable);
 	cell *tmp = clone_to_heap(q, true, p1, 1+p2->nbr_cells+1);
@@ -5179,6 +5183,7 @@ static USE_RESULT pl_state fn_if_2(query *q)
 	make_structure(tmp+nbr_cells++, g_cut_s, fn_soft_cut_0, 0, 0);
 	nbr_cells += copy_cells(tmp+nbr_cells, p2, p2->nbr_cells);
 	make_call(q, tmp+nbr_cells);
+	may_error(make_barrier(q));
 	q->st.curr_cell = tmp;
 	return pl_success;
 }
@@ -5272,7 +5277,7 @@ static USE_RESULT pl_state fn_iso_once_1(query *q)
 	idx_t nbr_cells = 1 + p1->nbr_cells;
 	make_structure(tmp+nbr_cells++, g_cut_s, fn_local_cut_0, 0, 0);
 	make_call(q, tmp+nbr_cells);
-	may_error(make_barrier(q));			// Why needed?
+	may_error(make_barrier(q));
 	q->st.curr_cell = tmp;
 	return pl_success;
 }
