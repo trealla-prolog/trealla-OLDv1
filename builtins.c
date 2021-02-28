1840682854ad747d7436ea3b7a9247e152d71c68
Andrew Davison <andrew@infradig.com>
2021-02-28 16:58:14 +1000

    Work on call/0 (call naked var)

--- builtins.c
+++ builtins.c
@@ -5000,6 +5000,9 @@ static USE_RESULT pl_state fn_iso_assertz_1(query *q)
 
 USE_RESULT pl_state fn_call_0(query *q, cell *p1)
 {
+	if (q->retry)
+		return pl_failure;
+
 	p1 = deref(q, p1, q->st.curr_frame);
 	idx_t p1_ctx = q->latest_ctx;
 
@@ -5021,6 +5024,7 @@ USE_RESULT pl_state fn_call_0(query *q, cell *p1)
 
 	idx_t nbr_cells = 0 + p1->nbr_cells;
 	make_call(q, tmp+nbr_cells);
+	may_error(make_barrier(q));
 	q->st.curr_cell = tmp;
 	return pl_success;
 }
