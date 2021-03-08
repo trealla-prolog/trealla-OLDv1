038c4292ff39de23a2fdb1242ccaa3dd59002ebf
Andrew Davison <andrew@infradig.com>
2021-03-09 08:54:33 +1000

    Fix compilation warning, re issue #181

--- src/builtins.c
+++ src/builtins.c
@@ -6133,7 +6133,7 @@ static cell *convert_to_list(query *q, cell *c, idx_t nbr_cells)
 {
 	if ((!nbr_cells || !c->nbr_cells)) {
 		cell *c = alloc_on_tmp(q, 1);
-		may_ptr_error(c);
+		if (!c) return c;
 		make_literal(c, g_nil_s);
 		return c;
 	}
