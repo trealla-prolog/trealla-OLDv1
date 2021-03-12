c72d2c679732f16042516b5b8d97e32e589aa5fc
Andrew Davison <andrew@infradig.com>
2021-03-13 09:25:42 +1000

    Cleanup printing code

--- src/print.c
+++ src/print.c
@@ -240,6 +240,9 @@ static unsigned count_non_anons(const uint8_t *mask, unsigned bit)
 
 ssize_t print_canonical_to_buf(query *q, char *dst, size_t dstlen, cell *c, idx_t c_ctx, int running, bool cons, unsigned depth)
 {
+	if (!running)
+		return print_term_to_buf(q, dst, dstlen, c, c_ctx, running, cons, depth);
+
 	if (!depth && !dst && !dstlen) {
 		fake_numbervars(q, c, c_ctx, 0);
 		memset(s_mask1, 0, MAX_ARITY);
@@ -399,16 +402,18 @@ ssize_t print_canonical_to_buf(query *q, char *dst, size_t dstlen, cell *c, idx_
 	return dst - save_dst;
 }
 
+static char *varformat(unsigned nbr)
+{
+	static char tmpbuf[80];
+	char *dst = tmpbuf;
+	dst += sprintf(dst, "%c", 'A'+nbr%26);
+	if ((nbr/26) > 0) sprintf(dst, "%u", nbr/26);
+	return tmpbuf;
+}
+
 #if 0
 ssize_t print_term_to_buf(query *q, char *dst, size_t dstlen, cell *c, idx_t c_ctx, int running, bool cons, unsigned depth)
 {
-	if (!depth && !dst && !dstlen) {
-		fake_numbervars(q, c, c_ctx, 0);
-		memset(s_mask1, 0, MAX_ARITY);
-		memset(s_mask2, 0, MAX_ARITY);
-		q->nv_start = -1;
-	}
-
 	char *save_dst = dst;
 
 	if (depth > MAX_DEPTH)
@@ -418,17 +423,14 @@ ssize_t print_term_to_buf(query *q, char *dst, size_t dstlen, cell *c, idx_t c_c
 		if (((c->flags & FLAG_HEX) || (c->flags & FLAG_BINARY))) {
 			dst += snprintf(dst, dstlen, "%s0x", c->val_num<0?"-":"");
 			dst += sprint_int(dst, dstlen, c->val_num, 16);
-		} else if ((c->flags & FLAG_OCTAL) && !running) {
-			dst += snprintf(dst, dstlen, "%s0o", c->val_num<0?"-":"");
-			dst += sprint_int(dst, dstlen, c->val_num, 8);
 		} else if (c->val_den != 1) {
 			if (q->flag.rational_syntax_natural) {
 				dst += sprint_int(dst, dstlen, c->val_num, 10);
-				dst += snprintf(dst, dstlen, "%s", "/");
+				dst += snprintf(dst, dstlen, "/");
 				dst += sprint_int(dst, dstlen, c->val_den, 10);
 			} else {
 				dst += sprint_int(dst, dstlen, c->val_num, 10);
-				dst += snprintf(dst, dstlen, "%s", " rdiv ");
+				dst += snprintf(dst, dstlen, " rdiv ");
 				dst += sprint_int(dst, dstlen, c->val_den, 10);
 			}
 		} else
@@ -449,7 +451,7 @@ ssize_t print_term_to_buf(query *q, char *dst, size_t dstlen, cell *c, idx_t c_c
 
 	if (is_float(c)) {
 		char tmpbuf[256];
-		sprintf(tmpbuf, "%.*g", DBL_DECIMAL_DIG, c->val_flt);
+		sprintf(tmpbuf, "%.*g", DBL_DECIMAL_DIG-1, c->val_flt);
 		const char *ptr = strchr(tmpbuf, '.');
 
 		if (ptr && (strlen(ptr+1) > 1))
@@ -460,116 +462,281 @@ ssize_t print_term_to_buf(query *q, char *dst, size_t dstlen, cell *c, idx_t c_c
 		return dst - save_dst;
 	}
 
-	idx_t var_nbr = 0;
+	int is_chars_list = scan_is_chars_list(q, c, c_ctx, 0);
 
-	if (is_variable(c)
-		&& (running>0) && (q->nv_start == -1)
-		&& ((var_nbr = find_binding(q, c->var_nbr, c_ctx)) != ERR_IDX)) {
-
-		for (unsigned i = 0; i < MAX_ARITY; i++) {
-			if (q->nv_mask[i])
-				break;
-
-			var_nbr--;
-		}
-
-		if (!dstlen) {
-			if (!(s_mask1[var_nbr]))
-				s_mask1[var_nbr] = 1;
-			else
-				s_mask2[var_nbr] = 1;
-		}
-
-		unsigned nbr = count_non_anons(s_mask2, var_nbr);
-
-		char ch = 'A';
-		ch += nbr % 26;
-		unsigned n = (unsigned)nbr / 26;
-
-		if (dstlen && !(s_mask2[var_nbr]))
-			dst += snprintf(dst, dstlen, "%s", "_");
-		else if (nbr < 26)
-			dst += snprintf(dst, dstlen, "%c", ch);
-		else
-			dst += snprintf(dst, dstlen, "%c%u", ch, n);
-
-		return dst - save_dst;
-	}
-
-	if (is_variable(c) && (running>0)) {
-		frame *g = GET_FRAME(c_ctx);
-		slot *e = GET_SLOT(g, c->var_nbr);
-		idx_t slot_nbr = e - q->slots;
-		dst += snprintf(dst, dstlen, "_%u", (unsigned)slot_nbr);
-		return dst - save_dst;
-	}
-
-	if (is_string(c)) {
-		int cnt = 0;
+	if (is_chars_list) {
 		cell *l = c;
-
+		dst += snprintf(dst, dstlen, "%s", "\"");
 		LIST_HANDLER(l);
 
 		while (is_list(l)) {
-			if ((cnt > 256) && (running < 0)) {
-				dst += snprintf(dst, dstlen, "|...");
-				return dst - save_dst;
-			}
-
 			cell *h = LIST_HEAD(l);
+			cell *c = deref(q, h, c_ctx);
+			dst += formatted(dst, dstlen, GET_STR(c), LEN_STR(c), false);
 			l = LIST_TAIL(l);
-			h->flags &= ~FLAG_STRING;
-
-			if (!cnt++)
-				allocate_list(q, h);
-			else
-				append_list(q, h);
+			l = deref(q, l, c_ctx);
+			c_ctx = q->latest_ctx;
 		}
 
-		c = end_list(q);
+		dst += snprintf(dst, dstlen, "%s", "\"");
+		return dst - save_dst;
 	}
+
+	// FIXME make non-recursive
 
 	const char *src = GET_STR(c);
-	int dq = 0, quote = !is_variable(c) && needs_quote(q->m, src, LEN_STR(c));
-	if (is_string(c)) dq = quote = 1;
-	dst += snprintf(dst, dstlen, "%s", quote?dq?"\"":"'":"");
+	unsigned print_list = 0, cnt = 0;
 
-	if (quote || q->quoted)
-		dst += formatted(dst, dstlen, src, LEN_STR(c), dq);
-	else
-		dst += plain(dst, dstlen, src, LEN_STR(c), dq);
+	while (is_iso_list(c)) {
+		if (cnt++ > MAX_DEPTH) {
+			return ~(dst - save_dst);
+		}
 
-	dst += snprintf(dst, dstlen, "%s", quote?dq?"\"":"'":"");
+		if (q->max_depth && (depth >= q->max_depth) && (running < 0)) {
+			dst += snprintf(dst, dstlen, "%s", "|...");
+			return dst - save_dst;
+		}
 
-	if (!is_structure(c))
-		return dst - save_dst;
+		LIST_HANDLER(c);
 
-	idx_t arity = c->arity;
-	dst += snprintf(dst, dstlen, "(");
+		cell *head = LIST_HEAD(c);
 
-	for (c++; arity--; c += c->nbr_cells) {
-		cell *tmp = running ? deref(q, c, c_ctx) : c;
-		ssize_t res = print_term_to_buf(q, dst, dstlen, tmp, q->latest_ctx, running, cons, depth+1);
+		if (!cons)
+			dst += snprintf(dst, dstlen, "%s", "[");
+
+		head = running ? deref(q, head, c_ctx) : head;
+		idx_t head_ctx = q->latest_ctx;
+		bool special_op = (!strcmp(GET_STR(head), ",")
+			|| !strcmp(GET_STR(head), ";")
+			|| !strcmp(GET_STR(head), "->")
+			|| !strcmp(GET_STR(head), "*->")
+			|| !strcmp(GET_STR(head), "-->"));
+		int parens = is_structure(head) && special_op;
+		if (parens) dst += snprintf(dst, dstlen, "%s", "(");
+		ssize_t res = print_term_to_buf(q, dst, dstlen, head, head_ctx, running, 0, depth+1);
 		if (res < 0) return -1;
 		dst += res;
+		if (parens) dst += snprintf(dst, dstlen, "%s", ")");
 
-		if (arity)
-			dst += snprintf(dst, dstlen, ",");
+		cell *tail = LIST_TAIL(c);
+		tail = running ? deref(q, tail, c_ctx) : tail;
+		c_ctx = q->latest_ctx;
+
+		if (is_literal(tail) && !is_structure(tail)) {
+			src = GET_STR(tail);
+
+			if (strcmp(src, "[]")) {
+				dst += snprintf(dst, dstlen, "%s", "|");
+				ssize_t res = print_term_to_buf(q, dst, dstlen, tail, c_ctx, running, 1, depth+1);
+				if (res < 0) return -1;
+				dst += res;
+			}
+		} else if (is_iso_list(tail)) {
+			dst += snprintf(dst, dstlen, "%s", ",");
+			c = tail;
+			print_list++;
+			cons = 1;
+			continue;
+		} else if (is_string(tail)) {
+			cell *l = tail;
+			LIST_HANDLER(l);
+
+			while (is_list(l)) {
+				dst += snprintf(dst, dstlen, "%s", ",");
+				cell *h = LIST_HEAD(l);
+				dst += formatted(dst, dstlen, GET_STR(h), LEN_STR(h), false);
+				l = LIST_TAIL(l);
+			}
+
+			print_list++;
+		} else {
+			dst += snprintf(dst, dstlen, "%s", "|");
+			ssize_t res = print_term_to_buf(q, dst, dstlen, tail, c_ctx, running, 1, depth+1);
+			if (res < 0) return -1;
+			dst += res;
+		}
+
+		if (!cons || print_list)
+			dst += snprintf(dst, dstlen, "%s", "]");
+
+		return dst - save_dst;
 	}
 
-	dst += snprintf(dst, dstlen, ")");
+	int optype = GET_OP(c);
+
+	if (q->ignore_ops || !optype || !c->arity) {
+		int quote = ((running <= 0) || q->quoted) && !is_variable(c) && needs_quote(q->m, src, LEN_STR(c));
+		int dq = 0, braces = 0, parens = 0;
+		if (is_string(c)) dq = quote = 1;
+		if (q->quoted < 0) quote = 0;
+		if ((c->arity == 1) && is_literal(c) && !strcmp(src, "{}")) braces = 1;
+
+		if (running && is_literal(c) && !strcmp(src, "$VAR") && q->numbervars && is_integer(c+1)) {
+			unsigned var_nbr = ((c+1)->val_num) - q->nv_start;
+			dst += snprintf(dst, dstlen, "%s", varformat(var_nbr));
+			return dst - save_dst;
+		}
+
+		if (running && is_variable(c) && q->variable_names) {
+			cell *l = q->variable_names;
+			idx_t l_ctx = q->variable_names_ctx;
+			LIST_HANDLER(l);
+
+			while (is_list(l)) {
+				cell *h = LIST_HEAD(l);
+				h = deref(q, h, l_ctx);
+				cell *name = h+1;
+				cell *val = h+2;
+
+				if (!strcmp(GET_STR(val), GET_STR(c))) {
+					dst += snprintf(dst, dstlen, "%s", GET_STR(name));
+					return dst - save_dst;
+				}
+
+				l = LIST_TAIL(l);
+				l = deref(q, l, l_ctx);
+				l_ctx = q->latest_ctx;
+			}
+		}
+
+		dst += snprintf(dst, dstlen, "%s", !braces&&quote?dq?"\"":"'":"");
+
+		if (parens)
+			dst += snprintf(dst, dstlen, "%s", "(");
+
+		if (running && is_variable(c)
+			&& ((c_ctx != q->st.curr_frame) || is_fresh(c) || (running > 0))) {
+			frame *g = GET_FRAME(c_ctx);
+			slot *e = GET_SLOT(g, c->var_nbr);
+			dst += snprintf(dst, dstlen, "_%u", (unsigned)(e - q->slots));
+			return dst - save_dst;
+		}
+
+		int len_str = LEN_STR(c);
+
+		if (braces)
+			;
+		else if (quote || q->quoted) {
+			if ((running < 0) && is_blob(c) && (len_str > 256))
+				len_str = 256;
+
+			dst += formatted(dst, dstlen, src, LEN_STR(c), dq);
+
+			if ((running < 0) && is_blob(c) && (len_str == 256))
+				dst += snprintf(dst, dstlen, "%s", "|...");
+		} else
+			dst += plain(dst, dstlen, src, LEN_STR(c), false);
+
+		dst += snprintf(dst, dstlen, "%s", !braces&&quote?dq?"\"":"'":"");
+
+		if (parens)
+			dst += snprintf(dst, dstlen, "%s", ")");
+
+		if (is_structure(c) && !is_string(c)) {
+			idx_t arity = c->arity;
+			dst += snprintf(dst, dstlen, "%s", braces?"{":"(");
+
+			for (c++; arity--; c += c->nbr_cells) {
+				cell *tmp = running ? deref(q, c, c_ctx) : c;
+				idx_t tmp_ctx = q->latest_ctx;
+				int parens = 0;
+
+				if (!braces && is_literal(tmp)) {
+					const char *s = GET_STR(tmp);
+
+					if (!strcmp(s, ",") || !strcmp(s, ";") ||
+						!strcmp(s, "->") || !strcmp(s, ":-") ||
+						!strcmp(s, "*->") || !strcmp(s, "-->"))
+						parens = 1;
+				}
+
+				if (parens)
+					dst += snprintf(dst, dstlen, "%s", "(");
+
+				ssize_t res = print_term_to_buf(q, dst, dstlen, tmp, tmp_ctx, running, 0, depth+1);
+				if (res < 0) return -1;
+				dst += res;
+
+				if (parens)
+					dst += snprintf(dst, dstlen, "%s", ")");
+
+				if (arity)
+					dst += snprintf(dst, dstlen, "%s", ",");
+			}
+
+			dst += snprintf(dst, dstlen, "%s", braces?"}":")");
+		}
+	} else if (IS_XF(c) || IS_YF(c)) {
+		cell *lhs = c + 1;
+		lhs = running ? deref(q, lhs, c_ctx) : lhs;
+		idx_t lhs_ctx = q->latest_ctx;
+		ssize_t res = print_term_to_buf(q, dst, dstlen, lhs, lhs_ctx, running, 0, depth+1);
+		if (res < 0) return -1;
+		dst += res;
+		dst += snprintf(dst, dstlen, "%s", src);
+	} else if (IS_FX(c) || IS_FY(c)) {
+		cell *rhs = c + 1;
+		rhs = running ? deref(q, rhs, c_ctx) : rhs;
+		idx_t rhs_ctx = q->latest_ctx;
+		int space = isalpha_utf8(peek_char_utf8(src)) || !strcmp(src, ":-") || !strcmp(src, "\\+");
+		space += !strcmp(src, "-") && is_rational(rhs) && (rhs->val_num < 0);
+		if (!strcmp(src, "-") && !is_rational(rhs)) dst += snprintf(dst, dstlen, "%s", " ");
+		int parens = is_structure(rhs) && !strcmp(GET_STR(rhs), ",");
+		dst += snprintf(dst, dstlen, "%s", src);
+		if (space && !parens) dst += snprintf(dst, dstlen, "%s", " ");
+		if (parens) dst += snprintf(dst, dstlen, "%s", "(");
+		ssize_t res = print_term_to_buf(q, dst, dstlen, rhs, rhs_ctx, running, 0, depth+1);
+		if (res < 0) return -1;
+		dst += res;
+		if (parens) dst += snprintf(dst, dstlen, "%s", ")");
+	} else {
+		cell *lhs = c + 1;
+		cell *rhs = lhs + lhs->nbr_cells;
+		lhs = running ? deref(q, lhs, c_ctx) : lhs;
+		idx_t lhs_ctx = q->latest_ctx;
+		rhs = running ? deref(q, rhs, c_ctx) : rhs;
+		idx_t rhs_ctx = q->latest_ctx;
+		int my_prec = get_op(q->m, GET_STR(c), NULL, false);
+		int lhs_prec1 = is_literal(lhs) ? get_op(q->m, GET_STR(lhs), NULL, false) : 0;
+		int lhs_prec2 = is_literal(lhs) && !lhs->arity ? get_op(q->m, GET_STR(lhs), NULL, false) : 0;
+		int rhs_prec1 = is_literal(rhs) ? get_op(q->m, GET_STR(rhs), NULL, false) : 0;
+		int rhs_prec2 = is_literal(rhs) && !rhs->arity ? get_op(q->m, GET_STR(rhs), NULL, false) : 0;
+		//printf("\n*** c=%s prec=%d\n", GET_STR(c), my_prec);
+		//printf("*** lhs=%s prec=%d\n", GET_STR(lhs), lhs_prec1);
+		//printf("*** rhs=%s prec=%d\n", GET_STR(rhs), rhs_prec1);
+		int parens = 0;//depth && strcmp(src, ",") && strcmp(src, "is") && strcmp(src, "->");
+		int lhs_parens = lhs_prec1 > my_prec;
+		lhs_parens |= lhs_prec2;
+		if (parens || lhs_parens) dst += snprintf(dst, dstlen, "%s", "(");
+		ssize_t res = print_term_to_buf(q, dst, dstlen, lhs, lhs_ctx, running, 0, depth+1);
+		if (res < 0) return -1;
+		dst += res;
+		if (lhs_parens) dst += snprintf(dst, dstlen, "%s", ")");
+		int space = isalpha_utf8(peek_char_utf8(src)) || !strcmp(src, ":-")
+			|| !strcmp(src, "-->") || !strcmp(src, "->") || !strcmp(src, "*->")
+			|| !strcmp(src, "=~=") || !strcmp(src, "=..")
+			|| !strcmp(src, "=>")|| !strcmp(src, "?=")
+			|| !*src;
+		if (space && !parens) dst += snprintf(dst, dstlen, "%s", " ");
+
+		dst += snprintf(dst, dstlen, "%s", src);
+		if (!*src) space = 0;
+		space += is_rational(rhs) && (rhs->val_num < 0);
+		if (space && !parens) dst += snprintf(dst, dstlen, "%s", " ");
+
+		int rhs_parens = rhs_prec1 > my_prec;
+		rhs_parens |= rhs_prec1 && lhs_prec1 && (rhs_prec1 != lhs_prec1);
+		rhs_parens |= rhs_prec2;
+		if (rhs_parens) dst += snprintf(dst, dstlen, "%s", "(");
+		res = print_term_to_buf(q, dst, dstlen, rhs, rhs_ctx, running, 0, depth+1);
+		if (res < 0) return -1;
+		dst += res;
+		if (parens || rhs_parens) dst += snprintf(dst, dstlen, "%s", ")");
+	}
+
 	return dst - save_dst;
 }
 #else
-static char *varformat(unsigned nbr)
-{
-	static char tmpbuf[80];
-	char *dst = tmpbuf;
-	dst += sprintf(dst, "%c", 'A'+nbr%26);
-	if ((nbr/26) > 0) sprintf(dst, "%u", nbr/26);
-	return tmpbuf;
-}
-
 ssize_t print_term_to_buf(query *q, char *dst, size_t dstlen, cell *c, idx_t c_ctx, int running, bool cons, unsigned depth)
 {
 	char *save_dst = dst;
