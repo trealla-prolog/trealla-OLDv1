31c64ff223539ac6b6d2fe28c747af830bded73d
Andrew Davison <andrew@infradig.com>
2021-03-30 14:55:09 +1000

    Cleanup attributes / unify hook

--- library/atts.pl
+++ library/atts.pl
@@ -5,21 +5,18 @@
 	'$undo_trail'(Vars),
 	'$process_vars'(Vars, [], Goals),
 	'$redo_trail',
-	maplist(call, Goals),
-	true.
+	maplist(call, Goals).
 
 '$process_vars'([], Goals, Goals) :- !.
 '$process_vars'([Var-Val|Vars], SoFar, Goals) :-
 	get_att(Var, Atts),
 	'$process_var'(Var, Val, Atts, SoFar, MoreGoals),
-	'$process_vars'(Vars, MoreGoals, Goals),
-	true.
+	'$process_vars'(Vars, MoreGoals, Goals).
 
 '$process_var'(_, _, [], Goals, Goals) :- !.
 '$process_var'(Var, Val, [Att|Atts], SoFar, Goals) :-
 	functor(Att, M, _),
 	M:verify_attributes(Var, Val, NewGoals),
 	'$append'(SoFar, NewGoals, MoreGoals),
-	'$process_var'(Var, Val, Atts, MoreGoals, Goals),
-	true.
+	'$process_var'(Var, Val, Atts, MoreGoals, Goals).
 
