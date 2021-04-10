242ce6a93d5d23177e25a259d113560c9772276c
Andrew Davison <andrew@infradig.com>
2021-04-10 10:12:07 +1000

    Cleanup

--- src/parse.c
+++ src/parse.c
@@ -34,7 +34,7 @@ idx_t g_empty_s, g_pair_s, g_dot_s, g_cut_s, g_nil_s, g_true_s, g_fail_s;
 idx_t g_anon_s, g_clause_s, g_eof_s, g_lt_s, g_gt_s, g_eq_s, g_false_s;
 idx_t g_sys_elapsed_s, g_sys_queue_s, g_braces_s, g_call_s, g_braces_s;
 idx_t g_stream_property_s, g_unify_s, g_on_s, g_off_s, g_sys_var_s;
-idx_t g_plus_s, g_minus_s, g_once_s, g_post_unify_hook_s;
+idx_t g_plus_s, g_minus_s, g_once_s, g_post_unify_hook_s, g_sys_recordkey_s;
 unsigned g_cpu_count = 4;
 char *g_tpl_lib = NULL;
 int g_ac = 0, g_avc = 1;
@@ -1731,6 +1731,11 @@ static void xref_cell(parser *p, term *t, cell *c, predicate *parent)
 
 void term_xref(parser *p, term *t, predicate *parent)
 {
+	cell *c = t->cells;
+
+	if (c->val_off == g_sys_recordkey_s)
+		return;
+
 	for (idx_t i = 0; i < t->cidx; i++) {
 		cell *c = t->cells + i;
 
@@ -4020,6 +4025,7 @@ static bool g_init(prolog *pl)
 			CHECK_SENTINEL(g_sys_var_s = index_from_pool(pl, "$VAR"), ERR_IDX);
 			CHECK_SENTINEL(g_stream_property_s = index_from_pool(pl, "$stream_property"), ERR_IDX);
 			CHECK_SENTINEL(g_post_unify_hook_s = index_from_pool(pl, "$post_unify_hook"), ERR_IDX);
+			CHECK_SENTINEL(g_sys_recordkey_s = index_from_pool(pl, "$recordkey"), ERR_IDX);
 		}
 
 		if (error) {
