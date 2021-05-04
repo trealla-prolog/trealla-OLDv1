e710c9afb366837fbc915028fab5c56e30b1e390
Andrew Davison <andrew@infradig.com>
2021-05-05 08:51:09 +1000

    Allow NULs in substrlen_utf8

--- src/utf8.c
+++ src/utf8.c
@@ -32,7 +32,7 @@ size_t substrlen_utf8(const char *s, size_t n)
 	const char *end = s + n;
 	size_t cnt = 0;
 
-	while (*s && (s <= end)) {
+	while (s < end) {
 		unsigned char ch = *(const unsigned char *)s++;
 
 		if ((ch < 0x80) || (ch > 0xBF))
