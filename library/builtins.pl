0dce353545cf72a6213dd98b80d9c7e013c6b11e
Andrew Davison <andrew@infradig.com>
2021-05-22 15:39:21 +1000

    Work on findall/3

--- library/builtins.pl
+++ library/builtins.pl
@@ -201,11 +201,6 @@ keysort(N, L1, L3, R) :-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
 
-findall(T, G, B) :-
-	copy_term('$findall'(T,G,B),G0),
-	'$rawcall'(G0),
-	'$findall'(T,G,B)=G0.
-
 findall(T, G, B, Tail) :-
 	'$mustbe_list_or_var'(B),
 	'$mustbe_list_or_var'(Tail),
