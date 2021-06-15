d04939c4a8c479dd95acb7c4dfb69862bd9d89ee
Andrew Davison <andrew@infradig.com>
2021-06-15 15:07:12 +1000

    Add imath in preparation

--- src/internal.h
+++ src/internal.h
@@ -130,6 +130,7 @@ typedef enum {
 #define get_numerator(c) (c)->val_int
 #define get_denominator(c) (c)->val_den
 
+#define is_bigint(c) (is_rational(c) && is_managed(c))
 #define is_atom(c) ((is_literal(c) && !(c)->arity) || is_cstring(c))
 #define is_string(c) (is_cstring(c) && (c)->flags & FLAG_STRING)
 #define is_managed(c) ((c)->flags & FLAG_MANAGED)
