2327e93b6eda27c478d9750651b72dd2006a04d7
Andrew Davison <andrew@infradig.com>
2021-06-17 14:44:50 +1000

    WIP - bigints

--- src/functions.c
+++ src/functions.c
@@ -1120,16 +1120,49 @@ static USE_RESULT pl_status fn_iso_powi_2(query *q)
 	cell p1 = calc(q, p1_tmp);
 	cell p2 = calc(q, p2_tmp);
 
-	if (is_integer(&p1) && is_integer(&p2)) {
+	if (is_bigint(&p1) && is_bigint(&p2)) {
+		if (mp_int_compare_value(&p1.val_big->rat.num, 1) != 0) {
+			if (mp_int_compare_value(&p2.val_big->rat.num, 0) < 0)
+				return throw_error(q, &p1, "type_error", "float");
+		}
+
+		mp_int_expt_full(&p1.val_big->rat.num, &p2.val_big->rat.num, &q->accum_rat.num);
+		SET_ACCUM();
+	} else if (is_bigint(&p1) && is_integer(&p2)) {
+		if (mp_int_compare_value(&p1.val_big->rat.num, 1) != 0) {
+			if (mp_int_compare_value(&p2.val_big->rat.num, 0) < 0)
+				return throw_error(q, &p1, "type_error", "float");
+		}
+
+		mp_int_expt(&p1.val_big->rat.num, p1.val_int, &q->accum_rat.num);
+		SET_ACCUM();
+	} else if (is_bigint(&p2) && is_integer(&p1)) {
 		if ((p1.val_int != 1) && (p2.val_int < 0))
 			return throw_error(q, &p1, "type_error", "float");
 
-		double d = pow(p1.val_int, p2.val_int);
+		mpz_t tmp;
+		mp_int_init_value(&tmp, p1.val_int);
+		mp_int_expt_full(&tmp, &p2.val_big->rat.num, &q->accum_rat.num);
+		mp_int_clear(&tmp);
+		SET_ACCUM();
+	} else if (is_integer(&p1) && is_integer(&p2)) {
+		if ((p1.val_int != 1) && (p2.val_int < 0))
+			return throw_error(q, &p1, "type_error", "float");
 
-		if (d > MY_INT64_MAX)
-			return throw_error(q, &p1, "evaluation_error", "int_overflow");
+		mpz_t tmp1, tmp2;
+		mp_int_init_value(&tmp1, p1.val_int);
+		mp_int_init_value(&tmp2, p2.val_int);
+		mp_int_expt_full(&tmp1, &tmp2, &q->accum_rat.num);
+		mp_int_clear(&tmp1);
+		mp_int_clear(&tmp2);
 
-		q->accum.val_int = d;
+		if (mp_int_compare_value(&q->accum_rat.num, MY_INT64_MAX) > 0) {
+			mp_int_set_value(&q->accum_rat.den, 1);
+			SET_ACCUM();
+			return pl_success;
+		}
+
+		q->accum.val_int = pow(p1.val_int, p2.val_int);
 		q->accum.val_type = TYPE_RATIONAL;
 	} else if (is_rational(&p1) && is_real(&p2)) {
 		q->accum.val_real = pow(p1.val_int, p2.val_real);
