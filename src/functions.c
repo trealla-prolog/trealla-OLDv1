e9adb211fa808438907a0c78aeef638c3b8471ca
Andrew Davison <andrew@infradig.com>
2021-06-17 12:38:58 +1000

    WIP - bigints

--- src/functions.c
+++ src/functions.c
@@ -1249,12 +1249,39 @@ static USE_RESULT pl_status fn_iso_mod_2(query *q)
 
 	if (is_bigint(&p1) && is_bigint(&p2)) {
 		mp_int_mod(&p1.val_big->rat.num, &p2.val_big->rat.num, &q->accum_rat.num);
+
+		if (mp_int_compare_zero(&p2.val_big->rat.num))
+			mp_int_neg(&q->accum_rat.num, &q->accum_rat.num);
+
+		if (mp_int_compare_zero(&p1.val_big->rat.num))
+			mp_int_neg(&q->accum_rat.num, &q->accum_rat.num);
+
 		SET_ACCUM();
 	} else if (is_bigint(&p1) && is_integer(&p2)) {
 		mpz_t tmp;
 		mp_int_init_value(&tmp, p2.val_int);
 		mp_int_mod(&p1.val_big->rat.num, &tmp, &q->accum_rat.num);
 		mp_int_clear(&tmp);
+
+		if (mp_int_compare_zero(&p2.val_big->rat.num))
+			mp_int_neg(&q->accum_rat.num, &q->accum_rat.num);
+
+		if (p1.val_int < 0)
+			q->accum.val_int *= -1;
+
+		SET_ACCUM();
+	} else if (is_bigint(&p2) && is_integer(&p1)) {
+		mpz_t tmp;
+		mp_int_init_value(&tmp, p1.val_int);
+		mp_int_mod(&tmp, &p2.val_big->rat.num, &q->accum_rat.num);
+		mp_int_clear(&tmp);
+
+		if (mp_int_compare_zero(&p1.val_big->rat.num))
+			mp_int_neg(&q->accum_rat.num, &q->accum_rat.num);
+
+		if (p2.val_int < 0)
+			q->accum.val_int *= -1;
+
 		SET_ACCUM();
 	} else if (is_integer(&p1) && is_bigint(&p2)) {
 		mpz_t tmp;
@@ -1354,14 +1381,26 @@ static USE_RESULT pl_status fn_iso_rem_2(query *q)
 	cell p1 = calc(q, p1_tmp);
 	cell p2 = calc(q, p2_tmp);
 
-	if (is_bigint(&p1) || is_bigint(&p2))
-		return throw_error(q, &p2, "type_error", "integer");
-
-	if (is_integer(&p1) && is_integer(&p2)) {
+	if (is_bigint(&p1) && is_bigint(&p2)) {
+		mp_int_mod(&p1.val_big->rat.num, &p2.val_big->rat.num, &q->accum_rat.num);
+		SET_ACCUM();
+	} else if (is_bigint(&p1) && is_integer(&p2)) {
+		mpz_t tmp;
+		mp_int_init_value(&tmp, p2.val_int);
+		mp_int_mod(&p1.val_big->rat.num, &tmp, &q->accum_rat.num);
+		mp_int_clear(&tmp);
+		SET_ACCUM();
+	} else if (is_integer(&p1) && is_bigint(&p2)) {
+		mpz_t tmp;
+		mp_int_init_value(&tmp, p1.val_int);
+		mp_int_mod(&tmp, &p2.val_big->rat.num, &q->accum_rat.num);
+		mp_int_clear(&tmp);
+		SET_ACCUM();
+	} else if (is_integer(&p1) && is_integer(&p2)) {
 		if (p2.val_int == 0)
 			return throw_error(q, &p1, "evaluation_error", "zero_divisor");
 
-		q->accum.val_int = (long long)(p1.val_int % p2.val_int);
+		q->accum.val_int = p1.val_int % p2.val_int;
 		q->accum.val_type = TYPE_RATIONAL;
 	} else if (is_variable(&p1) || is_variable(&p2)) {
 		return throw_error(q, &p1, "instantiation_error", "not_sufficiently_instantiated");
