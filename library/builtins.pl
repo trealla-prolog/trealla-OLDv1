73ee3010765881f2b20cdad6fbb87bc816aa888d
Andrew Davison <andrew@infradig.com>
2021-07-07 18:17:00 +1000

    WIP

--- library/builtins.pl
+++ library/builtins.pl
@@ -72,18 +72,18 @@ setup_call_cleanup(S, G, C) :-
 	'$chk_is_det'.
 
 catch(G, E, C) :-
-	'$call'('$catch'(G,E,C)).
-
-findall(T, G, B) :-
-	copy_term('$findall'(T,G,B), G0),
-	'$rawcall'(G0),
-	'$findall'(T,G,B)=G0.
+	'$call'('$catch'(G, E, C)).
 
 findall(T, G, B, Tail) :-
 	'$mustbe_list_or_var'(B),
 	'$mustbe_list_or_var'(Tail),
 	findall(T, G, B0),
 	append(B0, Tail, B), !.
+
+findall(T, G, B) :-
+	copy_term('$findall'(T,G,B), G0),
+	'$rawcall'(G0),
+	'$findall'(T,G,B)=G0.
 
 bagof(T, G, B) :-
 	copy_term('$bagof'(T,G,B), G0),
