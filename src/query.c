10b1fa281ab1baf011c30b0e3807e1f76be86fd1
Andrew Davison <andrew@infradig.com>
2021-07-20 16:22:24 +1000

    Disable rationalized variable dump

--- src/query.c
+++ src/query.c
@@ -1272,6 +1272,8 @@ static USE_RESULT pl_status match_head(query *q)
 
 static cell *check_duplicate_result(query *q, unsigned orig, cell *orig_c, idx_t orig_ctx, cell *tmp)
 {
+	return orig_c;
+
 	parser *p = q->p;
 	frame *g = GET_FRAME(0);
 	cell *c = orig_c;
@@ -1303,6 +1305,7 @@ static cell *check_duplicate_result(query *q, unsigned orig, cell *orig_c, idx_t
 			tmp->arity = 0;
 			tmp->flags = 0;
 			tmp->var_nbr = 0;
+			q->latest_ctx = orig_ctx;
 			return tmp;
 		}
 	}
