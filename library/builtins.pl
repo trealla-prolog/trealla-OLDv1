8a9465fb6b65fcdab344d1c7ae25caa14dc765e9
Andrew Davison <andrew@infradig.com>
2021-07-25 17:31:41 +1000

    Added strip_module/3

--- library/builtins.pl
+++ library/builtins.pl
@@ -372,7 +372,7 @@ prolog_load_context(module, Module) :- module(Module).
 open(F, M, S) :- open(F, M, S, []).
 load_files(Files) :- load_files(Files,[]).
 consult(Files) :- load_files(Files,[]).
-strip_module(T,M,P) :- T=M:P.
+strip_module(T,M,P) :- T=M:P -> true ; P=T, module(M).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % SWI compatible
