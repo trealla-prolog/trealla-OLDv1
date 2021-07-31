d5b8b9cb3b417b0c4f73b46da6ba96bbe517a873
Andrew Davison <andrew@infradig.com>
2021-07-31 13:21:07 +1000

    Fix a few clang compiler issues

--- src/internal.h
+++ src/internal.h
@@ -58,7 +58,8 @@ typedef enum {
 #define MAX_ARITY UCHAR_MAX
 #define MAX_QUEUES 16
 #define MAX_STREAMS 1024
-#define MAX_DEPTH 9000
+//#define MAX_DEPTH 9999
+#define MAX_DEPTH 3000			// Clang stack size needs this small
 #define STREAM_BUFLEN 1024
 
 #define GET_CHOICE(i) (q->choices+(i))
