773249fc552bf700df0baa41c40bd46adb1fcf06
Andrew Davison <andrew@infradig.com>
2021-08-05 12:39:19 +1000

    WIP

--- tests/tests/test058.pl
+++ tests/tests/test058.pl
@@ -1,9 +1,9 @@
 :-initialization(main).
 
-main :- sub_atom(abc,B,L,A,S),write([B,L,A,S]), nl, fail.
-main :- nl, sub_atom(abc,B,2,A,S),write([B,2,A,S]), nl, fail.
-main :- nl, sub_atom(abc,0,2,A,S),write([0,2,A,S]), nl, fail.
-main :- nl, sub_atom(abc,0,L,A,S),write([0,L,A,S]), nl, fail.
-main :- nl, sub_atom(abc,B,L,0,S),write([B,L,0,S]), nl, fail.
-main :- nl, sub_atom(abc,B,2,0,S),write([B,2,0,S]), nl, fail.
+main :- sub_atom(abc,B,L,A,S),writeq([B,L,A,S]), nl, fail.
+main :- nl, sub_atom(abc,B,2,A,S),writeq([B,2,A,S]), nl, fail.
+main :- nl, sub_atom(abc,0,2,A,S),writeq([0,2,A,S]), nl, fail.
+main :- nl, sub_atom(abc,0,L,A,S),writeq([0,L,A,S]), nl, fail.
+main :- nl, sub_atom(abc,B,L,0,S),writeq([B,L,0,S]), nl, fail.
+main :- nl, sub_atom(abc,B,2,0,S),writeq([B,2,0,S]), nl, fail.
 main :- halt.
