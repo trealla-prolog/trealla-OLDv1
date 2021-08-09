d8b481158e1f31506ea174f3c5025acca3981bd6
Andrew Davison <andrew@infradig.com>
2021-08-10 09:56:52 +1000

    Temporarily reduce (^2)/2 exponent to (2^32)/2, re issue #236

--- src/functions.c
+++ src/functions.c
@@ -1198,8 +1198,8 @@ static USE_RESULT pl_status fn_iso_powi_2(query *q)
 		if (p2.val_int < 0)
 			return throw_error(q, &p1, "type_error", "greater_zero");
 
-		if (p2.val_int > INT16_MAX)
-			return throw_error(q, &p1, "type_error", "range_error");
+		if (p2.val_int > (INT32_MAX/2))
+			return throw_error(q, &p1, "evaluation_error", "range_error");
 
 		if (mp_int_expt(&p1.val_bigint->ival, p2.val_int, &q->tmp_ival) != MP_OK)
 			return throw_error(q, &q->accum, "evaluation_error", "integer_overflow");
@@ -1211,8 +1211,8 @@ static USE_RESULT pl_status fn_iso_powi_2(query *q)
 		if ((p1.val_int != 1) && (p2.val_int < 0))
 			return throw_error(q, &p1, "type_error", "float");
 
-		if (p2.val_int > INT16_MAX)
-			return throw_error(q, &p1, "type_error", "range_error");
+		if (p2.val_int > (INT32_MAX/2))
+			return throw_error(q, &p1, "evaluation_error", "range_error");
 
 		if (mp_int_expt_value(p1.val_int, p2.val_int, &q->tmp_ival) != MP_OK)
 			return throw_error(q, &q->accum, "evaluation_error", "integer_overflow");
