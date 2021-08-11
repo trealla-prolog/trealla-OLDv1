0418177afb672fa0e956a01e0e3044b30c293582
Andrew Davison <andrew@infradig.com>
2021-08-11 19:23:54 +1000

    WIP

--- src/query.c
+++ src/query.c
@@ -207,8 +207,10 @@ static bool is_cyclic_term_internal(query *q, cell *p1, idx_t p1_ctx, ref *list)
 			cell *c = deref(q, p1, p1_ctx);
 			idx_t c_ctx = q->latest_ctx;
 
-			if (is_cyclic_term_internal(q, c, c_ctx, nlist))
+			if (is_cyclic_term_internal(q, c, c_ctx, nlist)) {
+				free(nlist);
 				return true;
+			}
 
 			free(nlist);
 		}
