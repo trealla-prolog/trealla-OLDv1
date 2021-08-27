856bb408e77a6dd82fba603375bd5747940f8c07
Andrew Davison <andrew@infradig.com>
2021-08-28 09:12:47 +1000

    WIP

--- src/parser.c
+++ src/parser.c
@@ -1009,7 +1009,9 @@ static cell *term_to_body_conversion(parser *p, cell *c)
 			if (is_variable(lhs)) {
 				c = insert_here(p, c, lhs);
 				lhs = c + 1;
-			} else
+			} else if (is_cstring(lhs))
+				convert_to_literal(p->m, lhs);
+			else
 				lhs = term_to_body_conversion(p, lhs);
 
 			cell *rhs = lhs + lhs->nbr_cells;
@@ -1017,6 +1019,8 @@ static cell *term_to_body_conversion(parser *p, cell *c)
 
 			if (is_variable(rhs))
 				c = insert_here(p, c, rhs);
+			else if (is_cstring(rhs))
+				convert_to_literal(p->m, rhs);
 			else
 				rhs = term_to_body_conversion(p, rhs);
 
@@ -1031,7 +1035,9 @@ static cell *term_to_body_conversion(parser *p, cell *c)
 			if (is_variable(rhs)) {
 				c = insert_here(p, c, rhs);
 				rhs = c + 1;
-			} else
+			} else if (is_cstring(rhs))
+				convert_to_literal(p->m, rhs);
+			else
 				rhs = term_to_body_conversion(p, rhs);
 
 			c->nbr_cells = 1 + rhs->nbr_cells;
