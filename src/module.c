670e4f4f7d5c131299ce466e8112908efe7f9356
Andrew Davison <andrew@infradig.com>
2021-09-01 11:35:47 +1000

    WIP

--- src/module.c
+++ src/module.c
@@ -210,7 +210,7 @@ static int index_compkey_internal(const void *ptr1, const void *ptr2, const void
 						return i;
 
 					break;
-				} else {
+				} else if (depth) {
 					int i = index_compkey_internal(p1, p2, param, args, depth+1);
 
 					if (i != 0)
