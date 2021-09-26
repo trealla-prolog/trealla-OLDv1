d9a91171b69d485d0fc6b9217fba51e12d6911f6
Andrew Davison <andrew@infradig.com>
2021-09-26 13:18:01 +1000

    WIP

--- src/control.c
+++ src/control.c
@@ -164,16 +164,17 @@ USE_RESULT pl_status fn_iso_invoke_2(query *q)
 
 USE_RESULT pl_status fn_iso_if_then_2(query *q)
 {
+	if (q->retry)
+		return pl_failure;
+
 	GET_FIRST_ARG(p1,callable);
 	GET_NEXT_ARG(p2,callable);
-	cell tmp1;
-	make_structure(&tmp1, g_once_s, NULL, 1, 0);
-	cell *tmp = clone_to_heap(q, true, &tmp1, p1->nbr_cells+p2->nbr_cells+1);
-	idx_t nbr_cells = 1;
-	tmp[nbr_cells++].nbr_cells += p1->nbr_cells;	// update the once structure
-	nbr_cells += safe_copy_cells(tmp+nbr_cells, p1, p1->nbr_cells);
+	cell *tmp = clone_to_heap(q, true, p1, 1+p2->nbr_cells+1);
+	idx_t nbr_cells = 1 + p1->nbr_cells;
+	make_structure(tmp+nbr_cells++, g_sys_inner_cut_s, fn_sys_inner_cut_0, 0, 0);
 	nbr_cells += safe_copy_cells(tmp+nbr_cells, p2, p2->nbr_cells);
 	make_call(q, tmp+nbr_cells);
+	may_error(make_barrier(q));
 	q->st.curr_cell = tmp;
 	return pl_success;
 }
