1bbdcebc6c9e721f45e17deefebd48d40d627c02
Andrew Davison <andrew@infradig.com>
2021-10-05 08:37:11 +1000

    WIP

--- src/parser.c
+++ src/parser.c
@@ -2815,10 +2815,10 @@ bool run(parser *p, const char *pSrc, bool dump, bool is_init)
 	if (!is_init) {
 		ASTRING(src);
 
-		ASTRING_sprintf(src, "_G_=(%s", pSrc);
+		ASTRING_sprintf(src, "call((%s)", pSrc);
 		ASTRING_trim_ws(src);
 		ASTRING_trim(src, '.');
-		ASTRING_strcat(src, "), _G_.");
+		ASTRING_strcat(src, ").");
 
 		p->srcptr = ASTRING_cstr(src);
 		p->line_nbr = 0;
