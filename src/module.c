25f036155d9d23372b619ce02dfb0bb0dc785c4e
Andrew Davison <andrew@infradig.com>
2021-10-05 11:42:37 +1000

    WIP

--- src/module.c
+++ src/module.c
@@ -974,7 +974,7 @@ module *load_text(module *m, const char *src, const char *filename)
 			p->consulting = false;
 			p->command = true;
 
-			if (run(p, "(:- initialization(G)), retract((:- initialization(_))), G", false, true))
+			if (run(p, "(:- initialization(G)), retract((:- initialization(_))), !, G", false, true))
 				p->m->pl->halt = true;
 		}
 
@@ -1032,7 +1032,7 @@ module *load_fp(module *m, FILE *fp, const char *filename)
 			p->command = true;
 			p->consulting = false;
 
-			if (run(p, "(:- initialization(G)), retract((:- initialization(_))), G", false, true))
+			if (run(p, "(:- initialization(G)), retract((:- initialization(_))), !, G", false, true))
 				p->m->pl->halt = true;
 		}
 
