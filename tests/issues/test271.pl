79dd66013e83251478b81bb7e61f33ad8a34126b
Andrew Davison <andrew@infradig.com>
2021-10-18 07:31:04 +1000

    Add findall test, re issue #271

---
+++ tests/issues/test271.pl
@@ -0,0 +1,20 @@
+a(1).
+a(2).
+a(_) :- throw(e).
+
+from_generator(Goal, Value, Optional) :-
+	catch(Goal, Error, true),
+	(	var(Error) ->
+		Optional = optional(Value)
+	;	Optional = empty,
+		!
+	).
+from_generator(_, _, empty).
+
+:-initialization(main).
+
+main :-
+	findall(Optional, from_generator(a(X), X, Optional), Optionals),
+	writeln(Optionals),
+	halt.
+
