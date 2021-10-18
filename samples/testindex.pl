d26ab149e278261d4f6361e70903f9753bf6a293
Andrew Davison <andrew@infradig.com>
2021-10-18 16:37:51 +1000

    Indexing work

--- samples/testindex.pl
+++ samples/testindex.pl
@@ -2,138 +2,125 @@
 :- dynamic(f/2).
 :- dynamic(f/1).
 
+main :-
+	test1a, test1b, test1c,
+	test2a, test2b,
+	test3,
+	test4,
+	test5.
+
 test1a :-
-	write('Load...'), nl,
 	between(1,1000000,I),
 		assertz(g(I,I)),
 		fail.
 test1a :-
-	write('Match using atomic 1st-arg...'), nl,
+	write('Match using atomic 1st-arg... '),
 	between(1,1000000,I),
 		g(I,_),
 		fail.
 test1a :-
 	abolish(g/2),
-	write('Done... '), write(1000000), write(' items'), nl, true.
+	write('Done...  '), write(1000000), write(' items'), nl, true.
 
 test1b :-
-	write('Load...'), nl,
 	between(1,1000000,I),
 		assertz(g(I,I)),
 		fail.
 test1b :-
-	write('Match using atomic 2nd-arg...'), nl,
+	write('Match using atomic 2nd-arg... '),
 	between(1,1000000,I),
 		g(_,I),
 		fail.
 test1b :-
 	abolish(g/2),
-	write('Done... '), write(1000000), write(' items'), nl, true.
+	write('Done...  '), write(1000000), write(' items'), nl, true.
 
 test1c :-
-	write('Load...'), nl,
 	between(1,1000000,I),
 		assertz(g(f(I),I)),
 		fail.
 test1c :-
-	write('Match using compound 1st-arg...'), nl,
+	write('Match using compound 1st-arg... '),
 	between(1,1000000,I),
 		g(f(I),_),
 		fail.
 test1c :-
 	abolish(g/2),
-	write('Done... '), write(1000000), write(' items'), nl, true.
+	write('Done...  '), write(1000000), write(' items'), nl, true.
 
 test1d :-
-	write('Load...'), nl,
 	between(1,1000000,I),
 		assertz(g(I,f(I))),
 		fail.
 test1d :-
-	write('Match using compound 2nd-arg...'), nl,
+	write('Match using compound 2nd-arg... '),
 	between(1,1000000,I),
 		g(_,f(I)),
 		fail.
 test1d :-
 	abolish(g/2),
-	write('Done... '), write(1000000), write(' items'), nl, true.
+	write('Done...  '), write(1000000), write(' items'), nl, true.
 
 test2a :-
-	write('Load...'), nl,
 	between(1,1000000,I),
 		assertz(f(I)),
 		fail.
 test2a :-
-	write('Iterate over set...'), nl,
+	write('Iterate over set... '),
 	f(_),
 		fail.
 test2a :-
 	abolish(f/1),
-	write('Done... '), write(1000000), write(' items'), nl, true.
+	write('Done...  '), write(1000000), write(' items'), nl, true.
 
 test2b :-
-	write('Load...'), nl,
 	between(1,1000000,I),
 		assertz(f(I)),
 		fail.
 test2b :-
-	write('Use findall...'), nl,
+	write('Use findall... '),
 	findall(N,f(N),L),
 	length(L,Count),
-	write('Done... '), write(Count), write(' items'), nl,
+	write('Done...  '), write(Count), write(' items'), nl,
 	true.
 
 test3 :-
-	write('Load...'), nl,
 	between(1,1000000,I),
 		assertz(g(I,I)),
 		fail.
 test3 :-
-	write('Iterate over 2nd-arg...'), nl,
+	write('Iterate over 2nd-arg... '),
 	g(_,_),
 		fail.
 test3 :-
 	abolish(f/1),
-	write('Done... '), write(1000000), write(' items'), nl,
+	write('Done...  '), write(1000000), write(' items'), nl,
 	true.
 
 test4 :-
-	write('Load...'), nl,
 	between(1,1000000,I),
 		assertz(f(I)),
 		fail.
 test4 :-
-	write('Retract...'), nl,
+	write('Retract... '),
 	retract(f(_)),
 		fail.
 test4 :-
 	abolish(f/1),
-	write('Done... '), write(1000000), write(' items'), nl, true.
+	write('Done...  '), write(1000000), write(' items'), nl, true.
 
 test5 :-
-	write('Load...'), nl,
 	between(1,10,_),
 		between(1,100000,J),
 			assertz(f(J)),
 			fail.
 test5 :-
-	write('Match using once 1st-arg...'), nl,
+	write('Match using once 1st-arg... '),
 	between(1,100000,I),
 		once(f(I)),
 		%write(I), nl,
 		fail.
 test5 :-
 	abolish(f/1),
-	write('Done... '), write(100000), write(' items'), nl, true.
-
-test6 :-
-	assertz(ff(3)),
-	assertz(ff(2)),
-	assertz(ff(1)),
-	ff(X),
-	write(X), nl,
-	fail.
-test6 :-
-	abolish(ff/1),
-	true.
+	write('Done...  '), write(100000), write(' items'), nl, true.
 
