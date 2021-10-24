522e61bfb6f9dbb4e3f1d11b403ec5bdd96fe512
Andrew Davison <andrew@infradig.com>
2021-10-25 07:19:44 +1000

    WIP

--- samples/factorial.pl
+++ samples/factorial.pl
@@ -1,7 +1,7 @@
 fac(N,F) :-
     factorial(N,1,F).
 
-factorial(0,F,F) :- !.
+factorial(0,F,F).
 factorial(N,Tot,F) :-
     NewTot is Tot * N,
     N1 is N - 1,
@@ -10,8 +10,9 @@ factorial(N,Tot,F) :-
 test :-
     fac(5,F1),
     F1 = 120,
-    write('fac(5)='), write(F1), write(' PASSED'), nl,
+    write('fac(5)='), write(F1), write(' PASSED'), nl.
 
+test1 :-
     fac(20,F2),
     F2 = 2432902008176640000,
     write('fac(20)='), write(F2), write(' PASSED'), nl.
