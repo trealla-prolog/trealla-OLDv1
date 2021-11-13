aaf1eaced5041344628c747ac155a1d467d0bb6a
Andrew Davison <andrew@infradig.com>
2021-11-13 18:09:32 +1000

    WIP

--- tests/tests/test080.pl
+++ tests/tests/test080.pl
@@ -5,7 +5,9 @@ main :-
 	freeze(V1, W1=2),
 	freeze(V2, W2=3),
 	V1 = V2,
-	V1 = 1,
-	writeln([V1,V2,W1,W2]).
+	(V1 = 1; V1 = -1),
+	writeln([V1,V2,W1,W2]),
+	fail.
+main.
 
 :- initialization(main).
