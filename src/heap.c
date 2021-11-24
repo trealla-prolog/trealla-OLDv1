b86c516a6392e744c6a977e5ff2c05fa5eb5655a
Andrew Davison <andrew@infradig.com>
2021-11-24 13:29:01 +1000

    Fix findall/3 with cyclic terms

--- src/heap.c
+++ src/heap.c
@@ -249,7 +249,7 @@ cell *do_deep_copy_to_heap(query *q, bool prefix, cell *p1, pl_idx_t p1_ctx, pl_
 
 static cell *deep_clone2_to_tmp(query *q, cell *p1, pl_idx_t p1_ctx, unsigned depth)
 {
-	if (depth >= MAX_DEPTH) {
+	if (depth >= (MAX_DEPTH*2)) {
 		q->cycle_error = true;
 		return ERR_CYCLE_CELL;
 	}
