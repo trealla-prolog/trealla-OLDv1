2ee1d714a93d953f4a475e376ee6459584c6e29c
Andrew Davison <andrew@infradig.com>
2021-12-11 14:13:06 +1000

    WIP

--- library/when.pl
+++ library/when.pl
@@ -6,18 +6,23 @@
 :- meta_predicate(when(+, 0)).
 :- attribute when/1.
 
-when(VarCond, Goal) :-
-	(	VarCond
-		-> 	Goal
-		;	(	term_variables(VarCond, Vars),
-				process_vars_(Vars, VarCond, Goal)
-			)
-	).
+when(nonvar(Var), Goal) :-
+	process_var_(Var, VarCond, Goal).
 
-process_vars_([], _, _).
-process_vars_([H|T], VarCond, Goal) :-
-	process_var_(H, VarCond, Goal),
-	process_vars_(T, VarCond, Goal).
+when(ground(Var), Goal) :-
+	process_var_(Var, VarCond, Goal).
+
+when(?=(Var1,Var2), Goal) :-
+	process_var_(Var1, VarCond, Goal),
+	process_var_(Var2, VarCond, Goal).
+
+when((Var1,Var2), Goal) :-
+	process_var_(Var1, VarCond, Goal),
+	process_var_(Var2, VarCond, Goal).
+
+when((Var1;Var2), Goal) :-
+	process_var_(Var1, VarCond, Goal),
+	process_var_(Var2, VarCond, Goal).
 
 process_var_(Var, VarCond, Goal) :-
 	(	get_atts(Var, when(OldCond-OldGoal))
