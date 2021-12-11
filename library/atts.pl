78c107c168c2f3228f038851cec52cb6c0f9c4d7
Andrew Davison <andrew@infradig.com>
2021-12-11 10:47:49 +1000

    WIP

--- library/atts.pl
+++ library/atts.pl
@@ -1,25 +1,24 @@
-:- module(atts,
-	[op(1150, fx, attribute)]).
+:- module(atts, [op(1150, fx, attribute)]).
 :- use_module(library(apply)).
 :- use_module(library(lists)).
 
 '$post_unify_hook' :-
 	'$undo_trail'(Vars),
-	'$process_vars'(Vars, [], Goals),
+	process_vars_(Vars, [], Goals),
 	'$redo_trail',
 	maplist(call, Goals),
 	!.
 
-'$process_vars'([], Goals, Goals) :- !.
-'$process_vars'([Var-Val|Vars], SoFar, Goals) :-
+process_vars_([], Goals, Goals) :- !.
+process_vars_([Var-Val|Vars], SoFar, Goals) :-
 	get_atts(Var, Atts),
-	'$process_var'(Var, Val, Atts, SoFar, MoreGoals),
-	'$process_vars'(Vars, MoreGoals, Goals).
+	process_var_(Var, Val, Atts, SoFar, MoreGoals),
+	process_vars_(Vars, MoreGoals, Goals).
 
-'$process_var'(_, _, [], Goals, Goals) :- !.
-'$process_var'(Var, Val, [Att|Atts], SoFar, Goals) :-
+process_var_(_, _, [], Goals, Goals) :- !.
+process_var_(Var, Val, [Att|Atts], SoFar, Goals) :-
 	functor(Att, M, _),
 	M:verify_attributes(Var, Val, NewGoals),
 	append(SoFar, NewGoals, MoreGoals),
-	'$process_var'(Var, Val, Atts, MoreGoals, Goals).
+	process_var_(Var, Val, Atts, MoreGoals, Goals).
 
