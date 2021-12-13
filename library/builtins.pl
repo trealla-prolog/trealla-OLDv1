e47b9698eaa1b77b8ae69b7480935ca96b43645a
Andrew Davison <andrew@infradig.com>
2021-12-13 17:40:13 +1000

    WIP

--- library/builtins.pl
+++ library/builtins.pl
@@ -718,17 +718,29 @@ current_op(A, B, C) :-
 put_attr(Var, Module, Value) :-
 	var(Var),
 	Attr =.. [Module,Value],
-	put_atts(Var, +Attr).
+	put_atts(Var, Attr).
 
 get_attr(Var, Module, Value) :-
 	var(Var),
 	Attr =.. [Module,Value],
-	get_atts(Var, +Attr).
+	get_atts(Var, Attr).
 
 del_attr(Var, Module) :-
 	var(Var),
 	Attr =.. [Module,_],
 	put_atts(Var, -Attr).
+
+% when goal expansion is implemented delete the above and use just this:
+
+goal_expansion(get_attr(Var, Module, Value), (var(Var),get_atts(Var, Access))) :-
+        Access =.. [Module,Value].
+
+goal_expansion(put_attr(Var, Module, Value), put_atts(Var, Access)) :-
+        Access =.. [Module,Value].
+
+goal_expansion(del_attr(Var, Module), (var(Var) -> put_atts(Var, -Access);true)) :-
+        Access =.. [Module,_].
+
 
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
