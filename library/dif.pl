6373f23fa358daa9f0186f1c854018655f53b7da
Andrew Davison <andrew@infradig.com>
2021-12-24 18:40:01 +1000

    WIP

--- library/dif.pl
+++ library/dif.pl
@@ -1,29 +1,17 @@
 :- module(dif, [dif/2]).
 
+% This is borrowed from Scryer-Prolog
+
 :- use_module(library(atts)).
 :- use_module(library(dcgs)).
 :- use_module(library(lists), [append/3]).
 
 :- attribute dif/1.
 
-dif(X, Y) :-
-    X \== Y,
-    (   X \= Y
-    ->	true
-    ;   (   term_variables(X, XVars),
-            term_variables(Y, YVars),
-            dif_set_variables(XVars, X, Y),
-            dif_set_variables(YVars, X, Y)
-        )
-    ).
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 put_dif_att(Var, X, Y) :-
-    (   get_atts(Var, +dif(Z))
-    ->	(	sort([X \== Y | Z], NewZ),
-			put_atts(Var, +dif(NewZ))
-		)
+    (   get_atts(Var, +dif(Z)) ->
+	    sort([X \== Y | Z], NewZ),
+	    put_atts(Var, +dif(NewZ))
     ;   put_atts(Var, +dif([X \== Y]))
     ).
 
@@ -34,21 +22,32 @@ dif_set_variables([Var|Vars], X, Y) :-
 
 append_goals([], _).
 append_goals([Var|Vars], Goals) :-
-    (   get_atts(Var, +dif(VarGoals))
-    ->	(	append(Goals, VarGoals, NewGoals0),
-			sort(NewGoals0, NewGoals)
-		)
+    (   get_atts(Var, +dif(VarGoals)) ->
+	    append(Goals, VarGoals, NewGoals0),
+	    sort(NewGoals0, NewGoals)
     ;   NewGoals = Goals
     ),
     put_atts(Var, +dif(NewGoals)),
     append_goals(Vars, Goals).
 
 verify_attributes(Var, Value, Goals) :-
-    (   get_atts(Var, +dif(Goals))
-    ->	(	term_variables(Value, ValueVars),
-			append_goals(ValueVars, Goals)
-		)
+    (   get_atts(Var, +dif(Goals)) ->
+	    term_variables(Value, ValueVars),
+	    append_goals(ValueVars, Goals)
     ;   Goals = []
+    ).
+
+% Probably the world's worst dif/2 implementation. I'm open to
+% suggestions for improvement.
+
+dif(X, Y) :-
+    X \== Y,
+    (   X \= Y -> true
+    ;   (   term_variables(X, XVars),
+            term_variables(Y, YVars),
+            dif_set_variables(XVars, X, Y),
+            dif_set_variables(YVars, X, Y)
+        )
     ).
 
 gather_dif_goals([]) --> [].
