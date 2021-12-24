ff30c72cf7c42426dc19099e4aee0bae70a85266
Andrew Davison <andrew@infradig.com>
2021-12-24 22:26:09 +1000

    Fix call_with_time_limit/2 to add meta_predicate directive

--- library/builtins.pl
+++ library/builtins.pl
@@ -551,7 +551,7 @@ recorded(K, V, R) :- nonvar(K), clause('$record_key'(K,V), _, R).
 
 call_with_time_limit(Time, Goal) :-
 	'$alarm'(Time),
-	(	once(Goal)
+	(	catch(once(Goal), E, ('$alarm'(0), throw(E)))
 	->	'$alarm'(0)
 	;	('$alarm'(0), fail)
 	).
