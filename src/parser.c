66d9bae63e10fff5edb2f9c7eba6b41d0c364cc6
Andrew Davison <andrew@infradig.com>
2022-01-07 09:19:55 +1000

    WIP

--- src/parser.c
+++ src/parser.c
@@ -1761,7 +1761,8 @@ static bool parse_number(parser *p, const char **srcptr, bool neg)
 	if (!isdigit(*s))
 		return false;
 
-	if ((*s == '0') && (s[1] == '\'') && !search_op(p->m, "", NULL, false)) {
+	if ((*s == '0') && (s[1] == '\'')
+		&& (!search_op(p->m, "", NULL, false) || ((s[2] == '\'') && (s[3] == '\'')))) {
 		s += 2;
 		int v;
 
