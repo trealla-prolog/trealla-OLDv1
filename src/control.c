75e2a32dc92374f3b30f39d5d5ba4978af45460f
Andrew Davison <andrew@infradig.com>
2022-01-09 13:00:26 +1000

    Fix off-by-one error in allocating some control constructs, re issue #99

--- src/control.c
+++ src/control.c
@@ -198,7 +198,7 @@ USE_RESULT pl_status fn_iso_if_then_2(query *q)
 
 	GET_FIRST_ARG(p1,callable);
 	GET_NEXT_ARG(p2,callable);
-	cell *tmp = clone_to_heap(q, true, p1, 1+p2->nbr_cells+2);
+	cell *tmp = clone_to_heap(q, true, p1, 1+p2->nbr_cells+3);
 	pl_idx_t nbr_cells = 1 + p1->nbr_cells;
 	make_structure(tmp+nbr_cells++, g_sys_inner_cut_s, fn_sys_inner_cut_0, 0, 0);
 	nbr_cells += safe_copy_cells(tmp+nbr_cells, p2, p2->nbr_cells);
@@ -219,7 +219,7 @@ static pl_status do_if_then_else(query *q, cell *p1, cell *p2, cell *p3)
 		return pl_success;
 	}
 
-	cell *tmp = clone_to_heap(q, true, p1, 1+p2->nbr_cells+2);
+	cell *tmp = clone_to_heap(q, true, p1, 1+p2->nbr_cells+3);
 	pl_idx_t nbr_cells = 1 + p1->nbr_cells;
 	make_structure(tmp+nbr_cells++, g_sys_inner_cut_s, fn_sys_inner_cut_0, 0, 0);
 	nbr_cells += safe_copy_cells(tmp+nbr_cells, p2, p2->nbr_cells);
@@ -239,7 +239,7 @@ USE_RESULT pl_status fn_if_2(query *q)
 
 	GET_FIRST_ARG(p1,callable);
 	GET_NEXT_ARG(p2,callable);
-	cell *tmp = clone_to_heap(q, true, p1, 1+p2->nbr_cells+2);
+	cell *tmp = clone_to_heap(q, true, p1, 1+p2->nbr_cells+3);
 	pl_idx_t nbr_cells = 1 + p1->nbr_cells;
 	make_structure(tmp+nbr_cells++, g_sys_soft_cut_s, fn_sys_soft_cut_0, 0, 0);
 	nbr_cells += safe_copy_cells(tmp+nbr_cells, p2, p2->nbr_cells);
@@ -260,7 +260,7 @@ static pl_status do_if_else(query *q, cell *p1, cell *p2, cell *p3)
 		return pl_success;
 	}
 
-	cell *tmp = clone_to_heap(q, true, p1, 1+p2->nbr_cells+2);
+	cell *tmp = clone_to_heap(q, true, p1, 1+p2->nbr_cells+3);
 	pl_idx_t nbr_cells = 1 + p1->nbr_cells;
 	make_structure(tmp+nbr_cells++, g_sys_soft_cut_s, fn_sys_soft_cut_0, 0, 0);
 	nbr_cells += safe_copy_cells(tmp+nbr_cells, p2, p2->nbr_cells);
