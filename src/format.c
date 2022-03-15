0b0e92b023361a9b3f22be4618cbd60f570e897b
Andrew Davison <andrew.davison@gmail.com>
2022-03-15 19:45:41 +1000

    Add ~Nw ~Nk ~Nq to set list depth to display

--- src/format.c
+++ src/format.c
@@ -516,6 +516,8 @@ pl_status do_format(query *q, cell *str, pl_idx_t str_ctx, cell *p1, pl_idx_t p1
 			if (is_string(c) && !q->quoted)
 				q->quoted = -1;
 
+			q->max_depth = !argval?26:argval;
+
 			if (canonical)
 				len = print_canonical_to_buf(q, NULL, 0, c, c_ctx, 1, false, 0);
 			else
@@ -533,6 +535,7 @@ pl_status do_format(query *q, cell *str, pl_idx_t str_ctx, cell *p1, pl_idx_t p1
 			else
 				len = print_term_to_buf(q, dst, len+1, c, c_ctx, 1, false, 0);
 
+			clear_write_options(q);
 			q->quoted = saveq;
             break;
         }
