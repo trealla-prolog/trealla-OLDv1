bb72a3cf5b791abf01049d5ccfb3968f48ac6819
Andrew Davison <andrew.davison@gmail.com>
2022-03-16 12:58:17 +1000

    Support copy_term/2 and cyclic_lists.

--- src/predicates.c
+++ src/predicates.c
@@ -4846,7 +4846,7 @@ static USE_RESULT pl_status fn_iso_copy_term_2(query *q)
 	GET_FIRST_ARG(p1,any);
 	GET_NEXT_ARG(p2,any);
 
-#define ALLOW_CYCLES 0
+#define ALLOW_CYCLES 1
 
 #if !ALLOW_CYCLES
 	bool is_partial = false;
@@ -4871,15 +4871,19 @@ static USE_RESULT pl_status fn_iso_copy_term_2(query *q)
 		return throw_error(q, p1, p1_ctx, "resource_error", "cyclic_term");
 
 #if ALLOW_CYCLES
-	if (!is_variable(p1)) {
-		pl_idx_t tmp_ctx = q->st.curr_frame;
-		pl_int_t skip = 0;
-		cell tmp2;
-		cell *t = skip_max_list(q, tmp, &tmp_ctx, 1000000000, &skip, &tmp2);
+	pl_idx_t tmp_ctx = p1_ctx;
+	pl_int_t skip = 0;
+	cell tmp2;
+	cell *t = skip_max_list(q, p1, &tmp_ctx, 1000000000, &skip, &tmp2);
 
-		if (t && is_variable(t) && !skip) {
-			unify(q, p2, p2_ctx, t, q->st.curr_frame);
-		}
+	//printf("*** t=%p, is_variable(t)=%d, skip=%d\n", (void*)t, is_variable(t), (int)skip);
+
+	if (t && !is_variable(t) && !skip) {
+		tmp_ctx = q->st.curr_frame;
+		skip = 0;
+		t = skip_max_list(q, tmp, &tmp_ctx, 1000000000, &skip, &tmp2);
+
+		unify(q, p2, p2_ctx, t, q->st.curr_frame);
 	}
 #endif
 
