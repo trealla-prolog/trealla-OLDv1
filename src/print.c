2785f137b762db36d1857eea3034d953b66c76d3
Andrew Davison <andrew.davison@gmail.com>
2022-03-19 06:26:13 +1000

    WIP

--- src/print.c
+++ src/print.c
@@ -468,8 +468,7 @@ ssize_t print_canonical_to_buf(query *q, char *dst, size_t dstlen, cell *c, pl_i
 			cell *name = h+1;
 			name = deref(q, name, h_ctx);
 			cell *var = h+2;
-			var = var;//deref(q, var, h_ctx);
-			pl_idx_t var_ctx = h_ctx;//q->latest_ctx;
+			pl_idx_t var_ctx = h_ctx;
 
 			if (is_variable(var) && (var->var_nbr == c->var_nbr) && (var_ctx = c_ctx)) {
 				dst += snprintf(dst, dstlen, "%s", GET_STR(q, name));
@@ -901,8 +900,7 @@ ssize_t print_term_to_buf(query *q, char *dst, size_t dstlen, cell *c, pl_idx_t 
 				cell *name = h+1;
 				name = deref(q, name, h_ctx);
 				cell *var = h+2;
-				var = var;//deref(q, var, h_ctx);
-				pl_idx_t var_ctx = h_ctx;//q->latest_ctx;
+				pl_idx_t var_ctx = h_ctx;
 
 				if (is_variable(var) && (var->var_nbr == c->var_nbr) && (var_ctx == c_ctx)) {
 					dst += snprintf(dst, dstlen, "%s", GET_STR(q, name));
