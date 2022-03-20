7fb2f247d11ff52d657a64c8c8c8c8c3a8400218
Andrew Davison <andrew.davison@gmail.com>
2022-03-20 16:48:53 +1000

    WIP

--- src/unify.c
+++ src/unify.c
@@ -805,12 +805,12 @@ bool unify(query *q, cell *p1, pl_idx_t p1_ctx, cell *p2, pl_idx_t p2_ctx)
 	if (is_iso_list(p1) && is_iso_list(p2)) {
 		if (check_list(q, p1, p1_ctx, &is_partial, NULL) && check_list(q, p2, p2_ctx, &is_partial, NULL)) {
 			q->lists_ok = true;
-			pl_status ok = unify_internal(q, p1, p1_ctx, p2, p2_ctx);
+			pl_status ok = unify_lists(q, p1, p1_ctx, p2, p2_ctx);
 			q->lists_ok = false;
 			return ok;
 		}
 	} else
-		q->lists_ok = true;
+		q->lists_ok = false;
 
 	cycle_info info1 = {0}, info2 = {0};
 	q->info1 = &info1;
