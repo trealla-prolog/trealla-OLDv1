36fc8e23089f42793c94ba739b814ca0ba066366
Andrew Davison <andrew.davison@gmail.com>
2022-03-28 21:54:51 +1000

    WIP

--- src/print.c
+++ src/print.c
@@ -884,10 +884,11 @@ ssize_t print_term_to_buf(query *q, char *dst, size_t dstlen, cell *c, pl_idx_t 
 		if (is_string(c)) dq = quote = 1;
 		if (q->quoted < 0) quote = 0;
 		if ((c->arity == 1) && is_literal(c) && !strcmp(src, "{}")) braces = 1;
+		cell *c1 = deref(q, c+1, c_ctx);
 
 		if (running && is_literal(c) && !strcmp(src, "$VAR")
-			&& q->numbervars && (!q->is_dump_vars || depth) && is_integer(deref(q, c+1, c_ctx))) {
-			unsigned var_nbr = get_smallint(deref(q, c+1, c_ctx)) - q->nv_start;
+			&& q->numbervars && (!q->is_dump_vars || depth) && is_integer(c1)) {
+			unsigned var_nbr = get_smallint(c1) - q->nv_start;
 			dst += snprintf(dst, dstlen, "%s", varformat(var_nbr));
 			return dst - save_dst;
 		}
