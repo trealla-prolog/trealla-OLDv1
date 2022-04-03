76070688bf05e8548e2ad93f94e532c80e95449d
Andrew Davison <andrew.davison@gmail.com>
2022-04-03 17:22:12 +1000

    WIP

--- src/internal.h
+++ src/internal.h
@@ -344,11 +344,15 @@ struct cell_ {
 				pl_status (*fn)(query*);
 				predicate *match;
 				uint16_t priority;		// used in parsing operators
+			};
 
-				struct {
-					cell *tmp_attrs;
-					pl_idx_t tmp_attrs_ctx;
-				};
+			void *val_spare5;
+		};
+
+		struct {
+			struct {
+				cell *tmp_attrs;
+				pl_idx_t tmp_attrs_ctx;
 			};
 
 			uint32_t val_off;			// used with TAG_VAR & TAG_LITERAL
