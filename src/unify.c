1de33f4df247f5572b3aeedc0bc4435006d85e1e
Andrew Davison <andrew.davison@gmail.com>
2022-04-03 22:11:21 +1000

    WIP

--- src/unify.c
+++ src/unify.c
@@ -265,6 +265,7 @@ static bool is_cyclic_term_internal(query *q, cell *p1, pl_idx_t p1_ctx, uint32_
 	p1++;
 
 	while (arity--) {
+#if 0
 		if (is_variable(p1)) {
 			frame *f = GET_FRAME(p1_ctx);
 			slot *e = GET_SLOT(f, p1->var_nbr);
@@ -282,6 +283,13 @@ static bool is_cyclic_term_internal(query *q, cell *p1, pl_idx_t p1_ctx, uint32_
 					return true;
 			}
 		}
+#else
+		cell *c = deref(q, p1, p1_ctx);
+		pl_idx_t c_ctx = q->latest_ctx;
+
+		if (is_cyclic_term_internal(q, c, c_ctx, depth+1))
+			return true;
+#endif
 
 		p1 += p1->nbr_cells;
 	}
