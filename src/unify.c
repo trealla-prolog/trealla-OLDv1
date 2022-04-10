8a506ef0ed2ca826a3aac2c12ed4688ecf0f5b3d
Andrew Davison <andrew.davison@gmail.com>
2022-04-10 18:45:32 +1000

    WIP

--- src/unify.c
+++ src/unify.c
@@ -1089,6 +1089,8 @@ bool unify_internal(query *q, cell *p1, pl_idx_t p1_ctx, cell *p2, pl_idx_t p2_c
 	return g_disp[p1->tag].fn(q, p1, p2);
 }
 
+// FIXME: rewrite this using efficient sweep/mark methodology...
+
 bool unify(query *q, cell *p1, pl_idx_t p1_ctx, cell *p2, pl_idx_t p2_ctx)
 {
 	if (is_iso_list(p1) && is_iso_list(p2)) {
