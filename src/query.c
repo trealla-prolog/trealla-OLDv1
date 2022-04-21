fc13b0453d9b14312e14f814998270c255d87472
Andrew Davison <andrew.davison@gmail.com>
2022-04-21 10:19:28 +1000

    WIP

--- src/query.c
+++ src/query.c
@@ -1062,9 +1062,6 @@ void set_var(query *q, const cell *c, pl_idx_t c_ctx, cell *v, pl_idx_t v_ctx)
 	if (c_attrs)
 		q->run_hook = true;
 
-	// The q->cp-1 is because we have to allow for the temporary
-	// choice-point we are in...
-
 	if (q->cp || c_attrs)
 		add_trail(q, c_ctx, c->var_nbr, c_attrs, c_attrs_ctx);
 
