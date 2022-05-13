78d68099f7f2c94b8b8ac99e272cdfac682a0259
Andrew Davison <andrew.davison@gmail.com>
2022-05-13 16:31:32 +1000

    WIP

--- library/builtins.pl
+++ library/builtins.pl
@@ -728,9 +728,7 @@ portray_atts_(Term) :-
 	copy_term(Term, Copy, Gs),
 	Term = Copy,
 	Gs = [Gs0],
-	(	list(Gs0) -> toconjunction(Gs0,Gs1)
-	;	Gs1 = Gs0
-	),
+	(list(Gs0) -> toconjunction(Gs0,Gs1); Gs1 = Gs0),
 	write_term(Gs1, [varnames(true)]).
 
 dump_attvars_([]) :- !.
