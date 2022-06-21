d212e9dc59209ee2bc5c22069599216575217380
Andrew Davison <andrew.davison@gmail.com>
2022-06-21 14:33:29 +1000

    Extended syntax for integers, re issue #593

--- src/parser.c
+++ src/parser.c
@@ -1840,8 +1840,9 @@ void read_integer(parser *p, mp_int v2, int base, const char *src,  const char *
 		p->tmpbuf = malloc(p->tmpbuf_size=256);
 
 	char *dst = p->tmpbuf;
+	int spaces = 0;
 
-	 while (*src) {
+	while (*src) {
 		if ((base == 2) && ((*src < '0') || (*src > '1')))
 			break;
 
@@ -1854,6 +1855,14 @@ void read_integer(parser *p, mp_int v2, int base, const char *src,  const char *
 		if ((base == 16) && !isxdigit(*src))
 			break;
 
+		if (spaces > 1) {
+			if (DUMP_ERRS || !p->do_read_term)
+				fprintf(stdout, "Error: syntax error, illegal character\n");
+			*srcptr = src;
+			p->error = true;
+			return;
+		}
+
 		*dst++ = *src++;
 
 		if ((size_t)(dst - p->tmpbuf) >= (p->tmpbuf_size-1)) {
@@ -1862,10 +1871,24 @@ void read_integer(parser *p, mp_int v2, int base, const char *src,  const char *
 			dst = p->tmpbuf + offset;
 		}
 
-		int last_ch = *src;
+		int last_ch = *src, cnt = 0;
 
-		while (isblank(*src) || (*src == '_'))
+		while (isblank(*src) || (*src == '_')) {
+			if (cnt) {
+				if (DUMP_ERRS || !p->do_read_term)
+					fprintf(stdout, "Error: syntax error, illegal character\n");
+				*srcptr = src;
+				p->error = true;
+				return;
+			}
+
 			last_ch = *src++;
+
+			if (last_ch == '_')
+				cnt++;
+			else if (last_ch == ' ')
+				spaces++;
+		}
 
 		if (last_ch == '_') {
 			p->srcptr = (char*)src;
