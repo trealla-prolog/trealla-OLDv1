3d60eb99f948fad2414fd113fb168272dfe5e6f0
Andrew Davison <andrew.davison@gmail.com>
2022-07-18 11:32:32 +1000

    Revert nth0/3 and nth1/3 fixes related to unification bug, previous fix cured it

--- library/lists.pl
+++ library/lists.pl
@@ -51,7 +51,7 @@ intersection([H|T], Y, [H|Z]) :- member(H, Y), !, intersection(T, Y, Z).
 intersection([_|T], Y, Z) :- intersection(T, Y, Z).
 
 nth1_(1, [Head|_], Head).
-nth1_(N, [_|T], Item) :-   %Seems to be a bug in unification here
+nth1_(N, [_|T], Item) :-
     nth1_(M, T, Item),
     N is M + 1.
 
@@ -67,7 +67,7 @@ nth1(N, Es, E) :-
 	nth1_(N, Es, E).
 
 nth0_(0, [Head|_], Head).
-nth0_(N, [_|T], Item) :-   %Seems to be a bug in unification here
+nth0_(N, [_|T], Item) :-
     nth0_(M, T, Item),
     N is M + 1.
 
