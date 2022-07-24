2d29b00f2a923d959934c9ce139b27ad1c3c598c
Andrew Davison <andrew.davison@gmail.com>
2022-07-24 10:56:38 +1000

    WIP

--- src/query.c
+++ src/query.c
@@ -1071,8 +1071,7 @@ void cut_me(query *q, bool inner_cut, bool soft_cut)
 		if (ch->register_cleanup && !ch->did_cleanup) {
 			ch->did_cleanup = true;
 			cell *c = ch->st.curr_cell;
-			c = deref(q, c, ch->st.curr_frame);
-			pl_idx_t c_ctx = q->latest_ctx;
+			pl_idx_t c_ctx = ch->st.curr_frame;
 			c = deref(q, c+1, c_ctx);
 			c_ctx = q->latest_ctx;
 			cell *tmp = deep_clone_to_heap(q, c, c_ctx);
